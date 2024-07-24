380f4a1d54bae501e479c2a1fc248bd5a247cf64
Elian Doran <contact@eliandoran.me>
2024-07-24 23:23:36 +0300

    client-ts: Adapt progress script

--- _check_ts_progress.sh
+++ _check_ts_progress.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
+cd src/public
 cloc HEAD \
     --git --md \
     --include-lang=javascript,typescript \
-    --found=filelist.txt \
-    --exclude-dir=public,libraries,views,docs
+    --found=filelist.txt
 
 grep -R \.js$ filelist.txt
 rm filelist.txt
