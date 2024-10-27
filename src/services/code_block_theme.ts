dbb5e0e97167808bca11dda4502765b624105ba4
Elian Doran <contact@eliandoran.me>
2024-10-27 23:46:03 +0200

    server: Add friendlier names for color themes

--- src/services/code_block_theme.ts
+++ src/services/code_block_theme.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import themeNames from "./code_block_theme_names.json" assert { type: "json" }
 
 export function listSyntaxHighlightingThemes() {
     const path = "node_modules/@highlightjs/cdn-assets/styles";
@@ -7,10 +8,15 @@ export function listSyntaxHighlightingThemes() {
         .filter((el) => el.endsWith(".min.css"))
         .map((name) => {
             const nameWithoutExtension = name.replace(".min.css", "");            
+            let title = nameWithoutExtension.replace(/-/g, " ");
+
+            if (title in themeNames) {
+                title = (themeNames as Record<string, string>)[title];
+            }
             
             return {
                 val: `default:${nameWithoutExtension}`,
-                title: nameWithoutExtension.replace(/-/g, " ")
+                title: title
             };
         });
     return allThemes;
