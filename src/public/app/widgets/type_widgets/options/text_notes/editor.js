15b4eacdca589cf3604ad454ab05b245bbeced83
Elian Doran <contact@eliandoran.me>
2024-11-09 21:35:37 +0200

    client: Change design of editor settings slightly

--- src/public/app/widgets/type_widgets/options/text_notes/editor.js
+++ src/public/app/widgets/type_widgets/options/text_notes/editor.js
@@ -4,17 +4,12 @@ import OptionsWidget from "../options_widget.js";
 
 const TPL = `
 <div class="options-section">
-    <h4>${t("editor.title")}</h4>
+    <h4>${t("editing.editor_type.label")}</h4>
     
-    <div class="form-group row">
-        <div class="col-6">
-            <label>${t("editing.editor_type.label")}</label>
-            <select class="editor-type-select form-select">
-                <option value="ckeditor-balloon">${t("editing.editor_type.floating")}</option>
-                <option value="ckeditor-classic">${t("editing.editor_type.fixed")}</option>
-            </select>
-        </div>
-    </div>
+    <select class="editor-type-select form-select">
+        <option value="ckeditor-balloon">${t("editing.editor_type.floating")}</option>
+        <option value="ckeditor-classic">${t("editing.editor_type.fixed")}</option>
+    </select>
 </div>`;
 
 export default class EditorOptions extends OptionsWidget {
