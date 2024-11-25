4d5f04de13911bd59d554eb13e64501428d19e7a
Elian Doran <contact@eliandoran.me>
2024-11-25 21:16:39 +0200

    chore(mermaid): set up single-chunk ELK

--- libraries/mermaid-elk/webpack.config.cjs
+++ libraries/mermaid-elk/webpack.config.cjs
@@ -1,12 +1,19 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
     mode: "production",
     entry: "./main.js",
     output: {
         library: "MERMAID_ELK",
-        path: path.resolve(__dirname, "dist"),
+        filename: "elk.min.js",
+        path: path.resolve(__dirname),
         libraryTarget: "umd",
         libraryExport: "default"
-    }
+    },
+    plugins: [
+        new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 1
+        })
+    ]
 }
