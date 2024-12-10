1268916ad7839ca147e8cef621ec90c996a7a580
Adorian Doran <adorian@esevo.ro>
2024-12-10 18:09:55 +0200

    client: date formatter utility: add the ability to format exclusively dates or times

--- src/public/app/utils/formatters.js
+++ src/public/app/utils/formatters.js
@@ -1,14 +1,22 @@
 /**
  * Formats the given date to a string based on the current locale.
  * @param {Date | number} date 
- * @param {"full" | "long" | "medium" | "short" | undefined} dateStyle 
- * @param {"full" | "long" | "medium" | "short" | undefined} tiemStyle 
+ * @param {"full" | "long" | "medium" | "short" | "none" | undefined} dateStyle 
+ * @param {"full" | "long" | "medium" | "short" | "none" | undefined} tiemStyle 
  */
-export function formatDate(date, dateStyle = "medium", tiemStyle = "medium") {
-    const formatter = new Intl.DateTimeFormat(navigator.language, {
-        dateStyle: "medium",
-        timeStyle: "medium"
-    });
+export function formatDate(date, dateStyle = "medium", timeStyle = "medium") {
+    const locale = navigator.language;
 
-    return formatter.format(date);
+    if (timeStyle === "none") {
+        // Format only the date
+        return date.toLocaleDateString(locale, {dateStyle});
+    } else if (dateStyle === "none") {
+        // Format only the time
+        return date.toLocaleTimeString(locale, {timeStyle});
+    } else {
+        // Format the date and time
+        const formatter = new Intl.DateTimeFormat(navigator.language, {dateStyle, timeStyle});
+        return formatter.format(date);
+    }
 }
+
