ebb53d77c5f3b12b17e644c370ae219f8ae22a75
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-12-16 20:24:40 +0100

    i18n: localize aria-label

--- src/public/app/widgets/dialogs/about.js
+++ src/public/app/widgets/dialogs/about.js
@@ -12,7 +12,7 @@ const TPL = `
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">${t("about.title")}</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="${t('about.close')}"></button>
             </div>
             <div class="modal-body">
                 <table class="table table-borderless text-nowrap">
