36cb07b2f90f405979b0c0b93011cffc010e0dee
Elian Doran <contact@eliandoran.me>
2024-12-19 20:57:37 +0200

    chore(client/ts): port services/search

--- src/public/app/services/search.ts
+++ src/public/app/services/search.ts
@@ -1,11 +1,11 @@
 import server from "./server.js";
 import froca from "./froca.js";
 
-async function searchForNoteIds(searchString) {
-    return await server.get(`search/${encodeURIComponent(searchString)}`);
+async function searchForNoteIds(searchString: string) {
+    return await server.get<string[]>(`search/${encodeURIComponent(searchString)}`);
 }
 
-async function searchForNotes(searchString) {
+async function searchForNotes(searchString: string) {
     const noteIds = await searchForNoteIds(searchString);
 
     return await froca.getNotes(noteIds);
