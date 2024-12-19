e8d1fe4e842419c4a43694b1cf1fe1b385293912
Elian Doran <contact@eliandoran.me>
2024-12-19 20:58:50 +0200

    chore(client/ts): port services/sync

--- src/public/app/services/sync.ts
+++ src/public/app/services/sync.ts
@@ -2,8 +2,15 @@ import { t } from './i18n.js';
 import server from './server.js';
 import toastService from "./toast.js";
 
+// TODO: De-duplicate with server once we have a commons.
+interface SyncResult {
+    success: boolean;
+    message: string;
+    errorCode?: string;
+}
+
 async function syncNow(ignoreNotConfigured = false) {
-    const result = await server.post('sync/now');
+    const result = await server.post<SyncResult>('sync/now');
 
     if (result.success) {
         toastService.showMessage(t("sync.finished-successfully"));
