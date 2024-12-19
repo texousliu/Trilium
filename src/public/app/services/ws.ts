9071a97730cf0b25438ec77fe42cc2f39a1edfb9
Elian Doran <contact@eliandoran.me>
2024-12-19 20:03:38 +0200

    chore(client/ts): fix one dependency to server

--- src/public/app/services/ws.ts
+++ src/public/app/services/ws.ts
@@ -4,8 +4,8 @@ import server from "./server.js";
 import options from "./options.js";
 import frocaUpdater from "./froca_updater.js";
 import appContext from "../components/app_context.js";
-import { EntityChange } from '../../../services/entity_changes_interface.js';
 import { t } from './i18n.js';
+import { EntityChange } from '../server_types.js';
 
 type MessageHandler = (message: any) => void;
 const messageHandlers: MessageHandler[] = [];
