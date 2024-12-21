05e49f77e685babc184b8aa4755dd0903af5c2fc
Elian Doran <contact@eliandoran.me>
2024-12-21 15:34:15 +0200

    chore(client/ts): remove unused type definition

--- src/public/app/services/attribute_parser.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'attribute_parser';
-
-
-export function lex(str: string): any[]
-export function parse(tokens: any[], str?: string, allowEmptyRelations?: boolean): any[]
-export function lexAndParse(str: string, allowEmptyRelations?: boolean): any[]
-
