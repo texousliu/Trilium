8667c0a6864b876a7abef5b6fc766c4ba3e44863
Elian Doran <contact@eliandoran.me>
2025-01-03 20:27:58 +0200

    refactor(server): split font route in two functions

--- src/routes/api/fonts.ts
+++ src/routes/api/fonts.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import optionService from "../../services/options.js";
+import { OptionMap } from '../../services/options_interface.js';
 
 function getFontCss(req: Request, res: Response) {
     res.setHeader('Content-Type', 'text/css');
@@ -12,27 +13,45 @@ function getFontCss(req: Request, res: Response) {
 
     const optionsMap = optionService.getOptionMap();
 
-    const mainFontFamilyOverridden = optionsMap.mainFontFamily !== 'theme';
-    const treeFontFamilyOverridden = optionsMap.treeFontFamily !== 'theme';
-    const detailFontFamilyOverridden = optionsMap.detailFontFamily !== 'theme';
-    const monospaceFontFamilyOverridden = optionsMap.monospaceFontFamily !== 'theme';
-
     // using body to be more specific than themes' :root
     let style = 'body {';
+    style += getFontFamily(optionsMap);
+    style += getFontSize(optionsMap);
+    style += '}';
 
-    if (mainFontFamilyOverridden) style += `--main-font-family: ${optionsMap.mainFontFamily};`;
-    if (treeFontFamilyOverridden) style += `--tree-font-family: ${optionsMap.treeFontFamily};`;
-    if (detailFontFamilyOverridden) style += `--detail-font-family: ${optionsMap.detailFontFamily};`;
-    if (monospaceFontFamilyOverridden) style += `--monospace-font-family: ${optionsMap.monospaceFontFamily};`;
+    res.send(style);
+}
 
+function getFontFamily(optionsMap: OptionMap) {
+    let style = "";
+
+    if (optionsMap.mainFontFamily !== 'theme') {
+        style += `--main-font-family: ${optionsMap.mainFontFamily};`;
+    }
+
+    if (optionsMap.treeFontFamily !== 'theme') {
+        style += `--tree-font-family: ${optionsMap.treeFontFamily};`;
+    }
+
+    if (optionsMap.detailFontFamily !== 'theme') {
+        style += `--detail-font-family: ${optionsMap.detailFontFamily};`;
+    }
+
+    if (optionsMap.monospaceFontFamily !== 'theme') {
+        style += `--monospace-font-family: ${optionsMap.monospaceFontFamily};`;
+    }
+
+    return style;
+}
+
+function getFontSize(optionsMap: OptionMap) {
+    let style = "";
     style += `--main-font-size: ${optionsMap.mainFontSize}%;`;
     style += `--tree-font-size: ${optionsMap.treeFontSize}%;`;
     style += `--detail-font-size: ${optionsMap.detailFontSize}%;`;
     style += `--monospace-font-size: ${optionsMap.monospaceFontSize}%;`;
 
-    style += '}';
-
-    res.send(style);
+    return style;
 }
 
 export default {
