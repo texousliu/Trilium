179299c2c738b18f8a0c2dc3076e358614dc9322
Raptor <github@0raptor.earth>
2025-01-05 13:53:46 +0100

    Enable Trilium to use RFC 5870 geo-URIs

--- packages/ckeditor5-build-trilium/src/config.ts
+++ packages/ckeditor5-build-trilium/src/config.ts
@@ -242,7 +242,7 @@ export const COMMON_SETTINGS = {
 			'http', 'https', 'ftp', 'ftps', 'mailto', 'data', 'evernote', 'file', 'facetime', 'gemini', 'git',
 			'gopher', 'imap', 'irc', 'irc6', 'jabber', 'jar', 'lastfm', 'ldap', 'ldaps', 'magnet', 'message',
 			'mumble', 'nfs', 'onenote', 'pop', 'rmi', 's3', 'sftp', 'skype', 'sms', 'spotify', 'steam', 'svn', 'udp',
-			'view-source', 'vlc', 'vnc', 'ws', 'wss', 'xmpp', 'jdbc', 'slack', 'tel', 'smb', 'zotero'
+			'view-source', 'vlc', 'vnc', 'ws', 'wss', 'xmpp', 'jdbc', 'slack', 'tel', 'smb', 'zotero', 'geo'
   		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
