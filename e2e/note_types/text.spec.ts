1530a09dc85c5a8479dd169bf238611f7cad29eb
Elian Doran <contact@eliandoran.me>
2025-01-11 13:41:25 +0200

    chore(e2e): check katex is rendered in ToC

--- e2e/note_types/text.spec.ts
+++ e2e/note_types/text.spec.ts
@@ -25,6 +25,10 @@ test("Table of contents is displayed", async ({ page }) => {
     await expect(rootList.locator("> ol").first().locator("> li").first()).toHaveText("Heading 1.1");
     await expect(rootList.locator("> ol").first().locator("> li").nth(1)).toHaveText("Heading 1.2");
 
+    // Heading 2 has a Katex equation, check if it's rendered.
+    await expect(rootList.locator("> li").nth(1)).toContainText("Heading 2");
+    await expect(rootList.locator("> li").nth(1).locator(".katex")).toBeAttached();
+
     await expect(rootList.locator("> ol")).toHaveCount(2);
     await expect(rootList.locator("> ol").nth(1).locator("> li")).toHaveCount(2);
     await expect(rootList.locator("> ol").nth(1).locator("> ol")).toHaveCount(1);
