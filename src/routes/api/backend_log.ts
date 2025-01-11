eb4b5a44df780ec6be96a6b888266a9978aa55a0
Panagiotis Papadopoulos <pano_90@gmx.net>
2025-01-11 01:06:13 +0100

    refactor(backend_log): use path.join for log file path

--- src/routes/api/backend_log.ts
+++ src/routes/api/backend_log.ts
@@ -1,14 +1,14 @@
 "use strict";
 
 import { readFile  } from "fs/promises";
+import { join } from "path";
 import dateUtils from "../../services/date_utils.js";
 import dataDir from "../../services/data_dir.js";
 const { LOG_DIR } = dataDir;
 
 async function getBackendLog() {
-    const file = `${LOG_DIR}/trilium-${dateUtils.localNowDate()}.log`;
-
     try {
+        const file = join(LOG_DIR, `trilium-${dateUtils.localNowDate()}.log`);
         return await readFile(file, "utf8");
     } catch (e) {
         // most probably the log file does not exist yet - https://github.com/zadam/trilium/issues/1977
