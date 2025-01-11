74b2643c283e2aa326077aba234319de4c6eb1b6
Elian Doran <contact@eliandoran.me>
2025-01-11 12:54:43 +0200

    chore(e2e): test eslint errors

--- e2e/note_types/code.spec.ts
+++ e2e/note_types/code.spec.ts
@@ -5,10 +5,9 @@ test("Displays lint warnings for backend script", async ({ page }) => {
     const app = new App(page);
     await app.goto();
     await app.closeAllTabs();
-    await app.goToNoteInNewTab("Backend script with lint warning");
+    await app.goToNoteInNewTab("Backend script with lint warnings");
 
     const codeEditor = app.currentNoteSplit.locator(".CodeMirror");
-    expect(await app.currentNoteSplit.locator(".note-title").inputValue()).toBe("Backend script with lint warnings");
 
     // Expect two warning signs in the gutter.
     expect(codeEditor.locator(".CodeMirror-gutter-wrapper .CodeMirror-lint-marker-warning")).toHaveCount(2);
@@ -20,6 +19,22 @@ test("Displays lint warnings for backend script", async ({ page }) => {
     // Hover over world
     await codeEditor.getByText("world").first().hover();
     await expectTooltip(page, "'world' is defined but never used.");
+});
+
+test("Displays lint errors for backend script", async ({ page }) => {
+    const app = new App(page);
+    await app.goto();
+    await app.closeAllTabs();
+    await app.goToNoteInNewTab("Backend script with lint errors");
+
+    const codeEditor = app.currentNoteSplit.locator(".CodeMirror");
+
+    // Expect two warning signs in the gutter.
+    expect(codeEditor.locator(".CodeMirror-gutter-wrapper .CodeMirror-lint-marker-error")).toHaveCount(1);
+
+    // Hover over hello
+    await codeEditor.getByText("world").first().hover();
+    await expectTooltip(page, "Parsing error: Unexpected token world");
 });
 
 async function expectTooltip(page: Page, tooltip: string) {
