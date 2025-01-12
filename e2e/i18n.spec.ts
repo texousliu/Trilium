c641ce26d1137c234d1b6629857c6d1d125aefb8
Elian Doran <contact@eliandoran.me>
2025-01-12 17:43:16 +0200

    chore(e2e): port old test for checking desktop language

--- e2e/i18n.spec.ts
+++ e2e/i18n.spec.ts
@@ -1,6 +1,14 @@
 import { test, expect, Page } from "@playwright/test";
 import App from "./support/app";
 
+test("Displays translation on desktop", async ({ page }) => {
+    const app = new App(page);
+    await app.goto();
+
+    await expect(page.locator("#left-pane .quick-search input"))
+        .toHaveAttribute("placeholder", "Quick search");
+});
+
 test("Displays translations in Settings", async ({ page }) => {
     const app = new App(page);
     await app.goto();
@@ -8,8 +16,8 @@ test("Displays translations in Settings", async ({ page }) => {
     await app.goToSettings();
     await app.noteTree.getByText("Appearance").click();
 
-    expect(app.currentNoteSplit).toContainText("Localization");
-    expect(app.currentNoteSplit).toContainText("Language");
+    await expect(app.currentNoteSplit).toContainText("Localization");
+    await expect(app.currentNoteSplit).toContainText("Language");
 });
 
 test("User can change language from settings", async ({ page }) => {
--- integration-tests/i18n.spec.ts
+++ integration-tests/i18n.spec.ts
@@ -1,10 +1,5 @@
 import test, { expect } from "@playwright/test";
 
-test("Restores language on start-up on desktop", async ({ page, context }) => {
-    await page.goto("http://localhost:8082");
-    await expect(page.locator("#launcher-pane").first()).toContainText("Open New Window");
-});
-
 test("Restores language on start-up on mobile", async ({ page, context }) => {
     await context.addCookies([
         {
