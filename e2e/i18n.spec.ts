dc83ba51dbb0b89b2511aadd038157ff743abeb3
Elian Doran <contact@eliandoran.me>
2025-01-12 17:36:10 +0200

    chore(e2e): port old test for changing language

--- e2e/i18n.spec.ts
+++ e2e/i18n.spec.ts
@@ -11,3 +11,24 @@ test("Displays translations in Settings", async ({ page }) => {
     expect(app.currentNoteSplit).toContainText("Localization");
     expect(app.currentNoteSplit).toContainText("Language");
 });
+
+test("User can change language from settings", async ({ page }) => {
+    const app = new App(page);
+    await app.goto();
+
+    await app.closeAllTabs();
+    await app.goToSettings();
+    await app.noteTree.getByText("Appearance").click();
+
+    // Check that the default value (English) is set.
+    await expect(app.currentNoteSplit).toContainText("Theme");
+    const languageCombobox = await app.currentNoteSplit.getByRole("combobox").first();
+    await expect(languageCombobox).toHaveValue("en");
+
+    // Select Chinese and ensure the translation is set.
+    languageCombobox.selectOption("cn");
+    await expect(app.currentNoteSplit).toContainText("主题");
+
+    // Select English again.
+    languageCombobox.selectOption("en");
+});
