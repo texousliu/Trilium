47335062a48b5bd1a60279327884db4e73de115d
Elian Doran <contact@eliandoran.me>
2025-01-17 23:21:12 +0200

    feat(e2e): test going back to share root

--- e2e/shared_notes.spec.ts
+++ e2e/shared_notes.spec.ts
@@ -8,3 +8,11 @@ test("Goes to share root", async ({ page, context }) => {
     await expect(page).toHaveTitle(noteTitle);
     await expect(page.locator("h1")).toHaveText(noteTitle);
 });
+
+test("Goes to parent share root page", async ({ page, context }) => {
+    const app = new App(page, context);
+    await app.goto({ url: "/share/bKMn5EFv9KS2" });
+    await expect(page.locator("h1")).toHaveText("Child note");
+    await page.locator("#parentLink a").click();
+    await page.waitForURL("/share/");
+});
