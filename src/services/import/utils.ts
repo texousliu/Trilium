2296d1a6ba351c861c8552ff6f3fdf3b453c4cac
Panagiotis Papadopoulos <pano_90@gmx.net>
2025-01-21 00:25:46 +0100

    refactor(import/utils.handleH1): simplify handleH1

--- src/services/import/utils.ts
+++ src/services/import/utils.ts
@@ -3,16 +3,17 @@
 function handleH1(content: string, title: string) {
     let isFirstH1Handled = false;
 
-    content = content.replace(/<h1[^>]*>([^<]*)<\/h1>/gi, (match, text) => {
-        if (title.trim() === text.trim() && !isFirstH1Handled) {
+    return content.replace(/<h1[^>]*>([^<]*)<\/h1>/gi, (match, text) => {
+        const convertedContent = `<h2>${text}</h2>`;
+
+        // strip away very first found h1 tag, if it matches the title
+        if (!isFirstH1Handled) {
             isFirstH1Handled = true;
-            return ""; // remove whole H1 tag
-        } else {
-            isFirstH1Handled = true;
-            return `<h2>${text}</h2>`;
+            return title.trim() === text.trim() ? "" : convertedContent;
         }
+
+        return convertedContent;
     });
-    return content;
 }
 
 function extractHtmlTitle(content: string): string | null {
