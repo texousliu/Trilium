668cc7e1a41ef43faafed64ff2d94d0cfd7f56ce
Panagiotis Papadopoulos <pano_90@gmx.net>
2025-01-22 07:38:25 +0100

    test(import/utils): prepare spec file

---
+++ src/services/utils.spec.ts
@@ -0,0 +1,75 @@
+import { describe, it, expect } from "vitest";
+import utils from "./utils.js";
+
+describe.todo("#newEntityId", () => {});
+
+describe.todo("#randomString", () => {});
+
+describe.todo("#randomSecureToken", () => {});
+
+describe.todo("#md5", () => {});
+
+describe.todo("#hashedBlobId", () => {});
+
+describe.todo("#toBase64", () => {});
+
+describe.todo("#fromBase64", () => {});
+
+describe.todo("#hmac", () => {});
+
+describe.todo("#isElectron", () => {});
+
+describe.todo("#hash", () => {});
+
+describe.todo("#isEmptyOrWhitespace", () => {});
+
+describe.todo("#sanitizeSqlIdentifier", () => {});
+
+describe.todo("#escapeHtml", () => {});
+
+describe.todo("#unescapeHtml", () => {});
+
+describe.todo("#toObject", () => {});
+
+describe.todo("#stripTags", () => {});
+
+describe.todo("#union", () => {});
+
+describe.todo("#escapeRegExp", () => {});
+
+describe.todo("#crash", () => {});
+
+describe.todo("#sanitizeFilenameForHeader", () => {});
+
+describe.todo("#getContentDisposition", () => {});
+
+describe.todo("#isStringNote", () => {});
+
+describe.todo("#quoteRegex", () => {});
+
+describe.todo("#replaceAll", () => {});
+
+// TriliumNextTODO move existing formatDownloadTitle in here
+// describe.todo("#formatDownloadTitle", () => {});
+
+describe.todo("#removeTextFileExtension", () => {});
+
+describe.todo("#getNoteTitle", () => {});
+
+describe.todo("#timeLimit", () => {});
+
+describe.todo("#deferred", () => {});
+
+describe.todo("#removeDiacritic", () => {});
+
+describe.todo("#normalize", () => {});
+
+describe.todo("#toMap", () => {});
+
+describe.todo("#isString", () => {});
+
+describe.todo("#getResourceDir", () => {});
+
+describe.todo("#isMac", () => {});
+
+describe.todo("#isWindows", () => {});
