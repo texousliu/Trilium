47b02da021d6186d02bcf02a0d1d619c6824f34a
Elian Doran <contact@eliandoran.me>
2025-01-22 20:02:20 +0200

    feat(geo_map): add back open context menu

--- src/public/app/widgets/type_widgets/geo_map_context_menu.ts
+++ src/public/app/widgets/type_widgets/geo_map_context_menu.ts
@@ -1,18 +1,25 @@
 import appContext from "../../components/app_context.js";
 import type { ContextMenuEvent } from "../../menus/context_menu.js";
 import contextMenu from "../../menus/context_menu.js";
+import linkContextMenu from "../../menus/link_context_menu.js";
 
 export default function openContextMenu(noteId: string, e: ContextMenuEvent) {
     contextMenu.show({
         x: e.pageX,
         y: e.pageY,
         items: [
+            ...linkContextMenu.getItems(),
+            { title: "----" },
             { title: "Remove from map", command: "deleteFromMap", uiIcon: "bx bx-trash" }
         ],
         selectMenuItemHandler: ({ command }) => {
-            if (command) {
+            if (command === "deleteFromMap") {
                 appContext.triggerCommand(command, { noteId });
+                return;
             }
+
+            // Pass the events to the link context menu
+            linkContextMenu.handleLinkContextMenuItem(command, noteId);
         }
     });
 }
