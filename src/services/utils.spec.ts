46f28f4f09ed12f69f0379bbc6e7f21b37c2b185
Panagiotis Papadopoulos <pano_90@gmx.net>
2025-01-31 21:39:42 +0100

    test(server/utils): add edge case test for getNoteTitle
    when the noteMeta title consists of just spaces, it should fall back to "normal" handling again → currently this fails

--- src/services/utils.spec.ts
+++ src/services/utils.spec.ts
@@ -270,6 +270,12 @@ describe("#getNoteTitle", () => {
           //@ts-expect-error - passing in incomplete noteMeta - but we only care about the title prop here
           ["test_file.md", true, { title: ""}],
           "test file"
+        ],
+        [
+            "when a noteMeta object is passed, but the title prop is empty, it should try to handle the filename as if no noteMeta was passed",
+            //@ts-expect-error - passing in incomplete noteMeta - but we only care about the title prop here
+            ["test_file.json", false, { title: " "}],
+            "test_file.json"
         ]
     ];
 
