1bf2f10c985da1ea4e932dc95cfd70046e5a572b
Elian Doran <contact@eliandoran.me>
2025-02-08 12:09:07 +0200

    feat(geo_map): enable retina detection

--- src/public/app/widgets/geo_map.ts
+++ src/public/app/widgets/geo_map.ts
@@ -54,7 +54,8 @@ export default class GeoMapWidget extends NoteContextAwareWidget {
                 }
 
                 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                    detectRetina: true
                 }).addTo(map);
             });
     }
