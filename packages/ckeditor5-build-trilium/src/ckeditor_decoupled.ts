7fbe4c10a595678242524ba30a3f92a11a1404a5
Elian Doran <contact@eliandoran.me>
2025-02-11 19:49:05 +0200

    refactor(editor): move classic toolbar config into Trilium repo

--- packages/ckeditor5-build-trilium/src/ckeditor_decoupled.ts
+++ packages/ckeditor5-build-trilium/src/ckeditor_decoupled.ts
@@ -15,62 +15,6 @@ export default class DecoupledEditor extends DecoupledEditorBase {
 	];
 
 	public static override defaultConfig = {
-		...COMMON_SETTINGS,
-
-		// For nested toolbars, refer to https://ckeditor.com/docs/ckeditor5/latest/getting-started/setup/toolbar.html#grouping-toolbar-items-in-dropdowns-nested-toolbars.
-		toolbar: {
-			items: [
-				'heading',
-				'fontSize',
-				'|',
-				'bold',
-				'italic',
-				{
-					label: "Text formatting",
-					icon: "text",
-					items: [
-						'underline',
-						'strikethrough',
-						'superscript',
-						'subscript',
-						'code',
-					],
-				},
-				'|',
-				'fontColor',
-				'fontBackgroundColor',
-				'removeFormat',
-				'|',
-				'bulletedList', 'numberedList', 'todoList',
-				'|',
-				'blockQuote',
-				'insertTable',
-				'codeBlock',
-				'footnote',
-				{
-					label: "Insert",
-					icon: "plus",
-					items: [
-						'imageUpload',
-						'|',
-						'link',
-						'internallink',
-						'includeNote',
-						'|',
-						'specialCharacters',
-						'math',
-						'mermaid',
-						'horizontalLine',
-						'pageBreak'
-					]
-				},
-				'|',
-				'outdent', 'indent',
-				'|',
-				'markdownImport',
-				'cuttonote',
-				'findAndReplace'
-			]
-		}
+		...COMMON_SETTINGS
 	};
 }
