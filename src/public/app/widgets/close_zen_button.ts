e829abbad3b918a838ce0f3581f7e89e80b28b48
Elian Doran <contact@eliandoran.me>
2025-02-14 18:47:15 +0200

    feat(zen): set up draggable regions

--- src/public/app/widgets/close_zen_button.ts
+++ src/public/app/widgets/close_zen_button.ts
@@ -2,13 +2,16 @@ import BasicWidget from "./basic_widget.js";
 
 const TPL = `\
 <div class="close-zen-container">
-    <a class="button-widget bx icon-action bxs-yin-yang"
+    <button class="button-widget bx icon-action bxs-yin-yang"
         data-trigger-command="toggleZenMode" />
 
     <style>
+    :root {
+        --zen-button-size: 32px;
+    }
+
     .close-zen-container {
         display: none;
-        --size: 32px;
         width: var(--size);
         height: var(--size);
     }
@@ -19,6 +22,7 @@ const TPL = `\
         top: 0;
         right: 0;
         z-index: 9999;
+        -webkit-app-region: no-drag;
     }
 
     body.zen.electron:not(.native-titlebar) .close-zen-container {
