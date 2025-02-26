6f9fd7646556e8b1052a81a38accad33236a37d6
Elian Doran <contact@eliandoran.me>
2025-02-26 20:33:18 +0200

    fix(share): double request of credentials

--- src/share/routes.spec.ts
+++ src/share/routes.spec.ts
@@ -1,20 +1,34 @@
-import { beforeAll, describe, it } from "vitest";
+import { beforeAll, beforeEach, describe, expect, it } from "vitest";
 import supertest from "supertest";
-import type { App } from "supertest/types.js";
 import { initializeTranslations } from "../services/i18n.js";
+import type { Application, Request, Response, NextFunction } from "express";
 
-let app: App;
+let app: Application;
 
 describe("Share API test", () => {
+    let cannotSetHeadersCount = 0;
+
     beforeAll(async () => {
         initializeTranslations();
         app = (await import("../app.js")).default;
+        app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+            if (err.message.includes("Cannot set headers after they are sent to the client")) {
+                cannotSetHeadersCount++;
+            }
+
+            next();
+        });
+    });
+
+    beforeEach(() => {
+        cannotSetHeadersCount = 0;
     });
 
     it("requests password for password-protected share", async () => {
         await supertest(app)
             .get("/share/YjlPRj2E9fOV")
             .expect("WWW-Authenticate", 'Basic realm="User Visible Realm", charset="UTF-8"');
+        expect(cannotSetHeadersCount).toBe(0);
     });
 
 });
