aed835b6c280b0517b46f6f49412a2f5e2fa3af4
Panagiotis Papadopoulos <pano_90@gmx.net>
2025-03-01 10:20:55 +0100

    chore: add initial eslint configuration
    rules will need some finetuning still

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    // consider using rules below, once we have a full TS codebase and can be more strict
+    // tseslint.configs.strictTypeChecked,
+    // tseslint.configs.stylisticTypeChecked,
+    //tseslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname
+            }
+        }
+    },
+    {
+        rules: {
+            // add rule overrides here
+        }
+    }
+);
