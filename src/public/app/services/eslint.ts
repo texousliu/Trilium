79adb0b20e2e6461e859659f9255cd21539d482b
Elian Doran <contact@eliandoran.me>
2025-03-08 02:45:40 +0200

    feat(client): add recommended rules

--- src/public/app/services/eslint.ts
+++ src/public/app/services/eslint.ts
@@ -1,9 +1,23 @@
 export async function lint(code: string) {
 
     const Linter = (await import("eslint-linter-browserify")).Linter;
+    const js = (await import("@eslint/js"));
+    const globals = (await import("globals"));
 
-    return new Linter().verify(code, {
-
-    });
+    return new Linter().verify(code, [
+        js.configs.recommended,
+        {
+            languageOptions: {
+                parserOptions: {
+                    ecmaVersion: 2024
+                },
+                globals: {
+                    ...globals.browser,
+                    api: "readonly"
+                }
+            },
+            rules: { }
+        }
+    ]);
 
 }
