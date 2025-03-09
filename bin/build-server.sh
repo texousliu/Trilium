1324cc2e53ef2b21fcbb3d4fb7596b20344dd2f7
Panagiotis Papadopoulos <pano_90@gmx.net>
2025-03-09 17:48:32 +0100

    build(build-server): add TODO remark

--- bin/build-server.sh
+++ bin/build-server.sh
@@ -62,6 +62,7 @@ rm -r $BUILD_DIR/node/lib/node_modules/npm \
 printf "#!/bin/sh\n./node/bin/node src/main" > $BUILD_DIR/trilium.sh
 chmod 755 $BUILD_DIR/trilium.sh
 
+# TriliumNextTODO: is this still required? If yes → move to copy-dist/copy-trilium
 cp bin/tpl/anonymize-database.sql $BUILD_DIR/
 
 cp -r translations $BUILD_DIR/
