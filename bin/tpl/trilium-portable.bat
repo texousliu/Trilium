df9a413ef836f6dccbf9eaf9bfccff859b2b98e7
PChemGuy <39730837+pchemguy@users.noreply.github.com>
2025-03-14 14:29:37 +0300

    Strip trailing backslash
    %~dp0 always contains a trailing backslash. The way %DIR% is used in the following line, the trailing slash should be stripped. Alternatively, `%DIR%\\` may be replaced with `%DIR%`, but I would advice against. It is ok to have `%~dp0trilium-data`, which is a conventional pattern. Once %~dp0 is replaced with %DIR% (which is not really warranted here), stripping the trailing slash right after assignment is preferable.

--- bin/tpl/trilium-portable.bat
+++ bin/tpl/trilium-portable.bat
@@ -15,6 +15,7 @@ chcp 65001
 
 :: Get Current Trilium executable directory and compute data directory
 SET DIR=%~dp0
+SET DIR=%DIR:~0,-1%
 SET TRILIUM_DATA_DIR=%DIR%\trilium-data
 cd "%DIR%"
 start trilium.exe
