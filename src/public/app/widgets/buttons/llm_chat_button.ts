5d0be300bffda0e2b021d1089f4fe849fd1526b5
Elian Doran <contact@eliandoran.me>
2025-03-16 23:28:52 +0200

    feat(llm): show/hide LLM button based on setting

--- src/public/app/widgets/buttons/llm_chat_button.ts
+++ src/public/app/widgets/buttons/llm_chat_button.ts
@@ -1,4 +1,6 @@
+import type { EventData } from "../../components/app_context.js";
 import type FNote from "../../entities/fnote.js";
+import options from "../../services/options.js";
 import CommandButtonWidget from "./command_button.js";
 
 export default class LlmChatButton extends CommandButtonWidget {
@@ -10,6 +12,16 @@ export default class LlmChatButton extends CommandButtonWidget {
             .title(() => note.title)
             .icon(() => note.getIcon())
             .class("launcher-button");
+    }
+
+    isEnabled() {
+        return options.get("aiEnabled") === "true";
+    }
+
+    entitiesReloadedEvent({ loadResults }: EventData<"entitiesReloaded">) {
+        if (loadResults.isOptionReloaded("aiEnabled")) {
+            this.refresh();
+        }
     }
 
 }
