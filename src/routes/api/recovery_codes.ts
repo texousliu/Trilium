c8164c81be18d61eb82922705376b455a4212d73
Jin <22962980+JYC333@users.noreply.github.com>
2025-03-22 12:47:49 +0100

    feat: 🎸 fix recovery_codes error

--- src/routes/api/recovery_codes.ts
+++ src/routes/api/recovery_codes.ts
@@ -1,21 +1,23 @@
-import recovery_codes from'../../services/encryption/recovery_codes.js';
-import {Request} from 'express';
-import {randomBytes} from 'crypto';
+import recovery_codes from '../../services/encryption/recovery_codes.js';
+import type { Request } from 'express';
+import { randomBytes } from 'crypto';
 
 function setRecoveryCodes(req: Request) {
     const success = recovery_codes.setRecoveryCodes(req.body.recoveryCodes);
-    return {success: success, message: 'Recovery codes set!'};
+    return { success: success, message: 'Recovery codes set!' };
 }
 
 function veryifyRecoveryCode(req: Request) {
     const success = recovery_codes.verifyRecoveryCode(req.body.recovery_code_guess);
 
-    return {success: success};
+    return { success: success };
 }
 
 function checkForRecoveryKeys() {
-    return {success
-: true, keysExist: recovery_codes.isRecoveryCodeSet()};
+    return {
+        success
+            : true, keysExist: recovery_codes.isRecoveryCodeSet()
+    };
 }
 
 function generateRecoveryCodes() {
@@ -32,7 +34,7 @@ function generateRecoveryCodes() {
 
     recovery_codes.setRecoveryCodes(recoveryKeys.toString());
 
-    return {success: true, recoveryCodes: recoveryKeys.toString()};
+    return { success: true, recoveryCodes: recoveryKeys.toString() };
 }
 
 function getUsedRecoveryCodes() {
