6f54ac7446df7f1fe5174ea1c2fc81ce58d883f8
Elian Doran <contact@eliandoran.me>
2025-03-24 12:05:46 +0200

    chore(ci): change signing log

--- bin/electron-forge/sign-windows.cjs
+++ bin/electron-forge/sign-windows.cjs
@@ -3,6 +3,6 @@ const SIGN_EXECUTABLE = "C:\\ev_signer_trilium\\ev_signer_trilium.exe";
 
 module.exports = function (filePath) {
     const command = `${SIGN_EXECUTABLE} --executable "${filePath}"`;
-    console.log(`> ${command}`);
+    console.log(`[Sign] ${command}`);
     child_process.execSync(command);
 }
