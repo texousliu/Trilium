34043a8a70206af7262d6a87027d1c6df7e7f406
Elian Doran <contact@eliandoran.me>
2025-03-24 17:00:37 +0200

    feat(signing): use environment variables for the executable path

--- bin/electron-forge/sign-windows.cjs
+++ bin/electron-forge/sign-windows.cjs
@@ -1,8 +1,14 @@
 const child_process = require("child_process");
-const SIGN_EXECUTABLE = "C:\\ev_signer_trilium\\ev_signer_trilium.exe";
 
 module.exports = function (filePath) {
-    const command = `${SIGN_EXECUTABLE} --executable "${filePath}"`;
+    const { WINDOWS_SIGN_EXECUTABLE } = process.env;
+
+    if (!WINDOWS_SIGN_EXECUTABLE) {
+        console.warn("[Sign] Skip signing due to missing environment variable.");
+        return;
+    }
+
+    const command = `${WINDOWS_SIGN_EXECUTABLE} --executable "${filePath}"`;
     console.log(`[Sign] ${command}`);
     child_process.execSync(command);
 }
