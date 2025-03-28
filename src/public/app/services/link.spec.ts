9585c6a55f1375ffda73fb5a81917ae7d7800ce2
Elian Doran <contact@eliandoran.me>
2025-03-28 22:22:42 +0200

    fix(client): "Note not found" tooltip for some URLs

--- src/public/app/services/link.spec.ts
+++ src/public/app/services/link.spec.ts
@@ -6,4 +6,14 @@ describe("Link", () => {
         const output = parseNavigationStateFromUrl("http://localhost:8080/#?searchString=hello");
         expect(output).toMatchObject({ searchString: "hello" });
     });
+
+    it("parses searchString with hash", () => {
+        const output = parseNavigationStateFromUrl("https://github.com/orgs/TriliumNext/discussions/1526#discussioncomment-12656660");
+        expect(output).toStrictEqual({});
+    });
+
+    it("parses notePath", () => {
+        const output = parseNavigationStateFromUrl(`#root/WWaBNf3SSA1b/mQ2tIzLVFKHL`);
+        expect(output).toMatchObject({ notePath: "root/WWaBNf3SSA1b/mQ2tIzLVFKHL", noteId: "mQ2tIzLVFKHL" });
+    });
 });
