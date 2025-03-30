827c228cdcb2581235b00c8c62bede892fb6a4d5
Elian Doran <contact@eliandoran.me>
2025-03-30 21:31:35 +0300

    feat(demo): add entrypoint for editing the demo safely

--- electron-docs-main.ts
+++ electron-docs-main.ts
@@ -3,79 +3,23 @@ import fsExtra from "fs-extra";
 import path from "path";
 import type NoteMeta from "./src/services/meta/note_meta.js";
 import type { NoteMetaFile } from "./src/services/meta/note_meta.js";
-import cls from "./src/services/cls.js";
 import { initializeTranslations } from "./src/services/i18n.js";
 import archiver, { type Archiver } from "archiver";
 import type { WriteStream } from "fs";
 import debounce from "./src/public/app/services/debounce.js";
+import { importData, initializeDatabase, startElectron } from "./electron-utils.js";
 
 const NOTE_ID_USER_GUIDE = "pOsGYCXsbNQG";
 const markdownPath = path.join("docs", "User Guide");
 const htmlPath = path.join("src", "public", "app", "doc_notes", "en", "User Guide");
 
-async function startElectron() {
-    await import("./electron-main.js");
-}
-
 async function main() {
     await initializeTranslations();
     const zipBuffer = await createImportZip();
     await initializeDatabase();
-    await importData(zipBuffer);
+    await importData(zipBuffer, NOTE_ID_USER_GUIDE);
     await startElectron();
     await registerHandlers();
-}
-
-async function initializeDatabase() {
-    const sqlInit = (await import("./src/services/sql_init.js")).default;
-
-    cls.init(() => {
-        if (!sqlInit.isDbInitialized()) {
-            sqlInit.createInitialDatabase();
-        }
-    });
-}
-
-function importData(input: Buffer) {
-    return new Promise<void>((resolve, reject) => {
-        cls.init(async () => {
-            const beccaLoader = ((await import("./src/becca/becca_loader.js")).default);
-            const notes = ((await import("./src/services/notes.js")).default);
-            beccaLoader.load();
-            const becca = ((await import("./src/becca/becca.js")).default);
-            const utils = ((await import("./src/services/utils.js")).default);
-            const eraseService = ((await import("./src/services/erase.js")).default);
-            const deleteId = utils.randomString(10);
-
-            const existingNote = becca.getNote(NOTE_ID_USER_GUIDE);
-            if (existingNote) {
-                existingNote.deleteNote(deleteId);
-            }
-            eraseService.eraseNotesWithDeleteId(deleteId);
-
-            const { note } = notes.createNewNoteWithTarget("into", "none_root", {
-                parentNoteId: "root",
-                noteId: NOTE_ID_USER_GUIDE,
-                title: "User Guide",
-                content: "The sub-children of this note are automatically synced.",
-                type: "text"
-            });
-
-            const TaskContext = (await import("./src/services/task_context.js")).default;
-            const { importZip } = ((await import("./src/services/import/zip.js")).default);
-            const context = new TaskContext("no-report");
-            await importZip(context, input, note, { preserveIds: true });
-
-            const { runOnDemandChecks } = (await import("./src/services/consistency_checks.js")).default;
-            await runOnDemandChecks(true);
-
-            becca.reset();
-            beccaLoader.load();
-
-            resolve();
-        });
-    });
-
 }
 
 async function createImportZip() {
---
+++ electron-edit-demo.ts
@@ -0,0 +1,43 @@
+import { importData, initializeDatabase, startElectron } from "./electron-utils.js";
+import { initializeTranslations } from "./src/services/i18n.js";
+import fs from "fs";
+import debounce from "./src/public/app/services/debounce.js";
+
+const DEMO_NOTE_ID = "demo";
+const DEMO_ZIP_PATH = "db/demo.zip";
+
+async function main() {
+    await initializeTranslations();
+    await initializeDatabase();
+
+    const demoBuffer = fs.readFileSync(DEMO_ZIP_PATH);
+    await importData(demoBuffer, DEMO_NOTE_ID);
+
+    await startElectron();
+    await registerHandlers();
+}
+
+async function registerHandlers() {
+    const events = (await import("./src/services/events.js")).default;
+    const eraseService = (await import("./src/services/erase.js")).default;
+    const debouncer = debounce(async () => {
+        console.log("Exporting data");
+        eraseService.eraseUnusedAttachmentsNow();
+        await exportData();
+    }, 10_000);;
+    events.subscribe(events.ENTITY_CHANGED, async (e) => {
+        if (e.entityName === "options") {
+            return;
+        }
+
+        console.log("Got entity changed ", e);
+        debouncer();
+    });
+}
+
+async function exportData() {
+    const { exportToZipFile } = (await import("./src/services/export/zip.js")).default;
+    await exportToZipFile(DEMO_NOTE_ID, "html", DEMO_ZIP_PATH);
+}
+
+await main();
