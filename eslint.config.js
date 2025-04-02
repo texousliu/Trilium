a0fe60eff1bfb9a9952683c5c26a4588191f0232
Panagiotis Papadopoulos <pano_90@gmx.net>
2025-04-02 08:35:24 +0200

    chore(eslint): move formatting rules to own config file for now
    this will allow us to run linting and formatting separately for now, where we have a ton of linting errors still

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,5 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
-import stylistic from "@stylistic/eslint-plugin";
-
-
-// Go to https://eslint.style/rules/default/${rule_without_prefix} to check the rule details
-const stylisticRules = {
-    "@stylistic/indent": [ "error", 4 ],
-    "@stylistic/quotes": [ "error", "double", { avoidEscape: true, allowTemplateLiterals: "always" } ],
-    "@stylistic/semi": [ "error", "always" ],
-    "@stylistic/quote-props": [ "error", "consistent-as-needed" ],
-    "@stylistic/max-len": [ "error", { code: 200 } ],
-    "@stylistic/comma-dangle": [ "error", "never" ],
-    "@stylistic/linebreak-style": [ "error", "unix" ],
-    "@stylistic/array-bracket-spacing": [ "error", "always" ],
-    "@stylistic/object-curly-spacing": [ "error", "always" ],
-    "@stylistic/padded-blocks": [ "error", { classes: "always" } ]
-};
 
 export default tseslint.config(
     eslint.configs.recommended,
@@ -46,8 +30,7 @@ export default tseslint.config(
                     argsIgnorePattern: "^_",
                     varsIgnorePattern: "^_"
                 }
-            ],
-            ...stylisticRules
+            ]
         }
     },
     {
