cdb5ebb08076c9b739111f19d61109f940e138a7
Elian Doran <contact@eliandoran.me>
2025-04-05 12:37:06 +0300

    feat(import/markdown): preserve trailing semicolon in figure style

--- src/services/import/markdown.spec.ts
+++ src/services/import/markdown.spec.ts
@@ -164,8 +164,8 @@ second line 2</code></pre><ul><li>Hello</li><li>world</li></ul><ol><li>Hello</li
     });
 
     it("preserves figures", () => {
-        const input = `<figure class="image"><img style="aspect-ratio:991/403;" src="Jump to Note_image.png" width="991" height="403"></figure>`;
-        const expected = /*html*/`<figure class="image"><img style="aspect-ratio:991/403;" src="Jump to Note_image.png" width="991" height="403"></figure>`;
+        const input = `<figure class="image image-style-align-center image_resized" style="width:50%;"><img style="aspect-ratio:991/403;" src="Jump to Note_image.png" width="991" height="403"></figure>`;
+        const expected = /*html*/`<figure class="image image-style-align-center image_resized" style="width:50%;"><img style="aspect-ratio:991/403;" src="Jump to Note_image.png" width="991" height="403"></figure>`;
         expect(markdownService.renderToHtml(input, "Title")).toStrictEqual(expected);
     });
 
