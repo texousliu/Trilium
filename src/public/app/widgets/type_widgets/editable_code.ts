d6478c2fed95b48a84ec12b922d744c32233dd5b
Elian Doran <contact@eliandoran.me>
2025-04-13 22:08:26 +0300

    fix(touchbar): errors refreshing touchbar if parent is missing

--- src/public/app/widgets/type_widgets/editable_code.ts
+++ src/public/app/widgets/type_widgets/editable_code.ts
@@ -65,7 +65,9 @@ export default class EditableCodeTypeWidget extends AbstractCodeTypeWidget {
 
         this.show();
 
-        this.triggerCommand("refreshTouchBar");
+        if (this.parent) {
+            this.triggerCommand("refreshTouchBar");
+        }
     }
 
     getData() {
