9b188503dba94c601032873f45da7d48b1562c80
Arne Keller <arne.keller@posteo.de>
2025-04-13 19:40:33 +0200

    Fix version number in API docs
    TODO: get this automatically?

--- _regroup/bin/generate-openapi.ts
+++ _regroup/bin/generate-openapi.ts
@@ -16,7 +16,7 @@ const options = {
         openapi: "3.1.1",
         info: {
             title: "Trilium Notes - Sync server API",
-            version: "0.96.6",
+            version: "0.93.0",
             description:
                 "This is the internal sync server API used by Trilium Notes / TriliumNext Notes.\n\n_If you're looking for the officially supported External Trilium API, see [here](https://triliumnext.github.io/Docs/Wiki/etapi.html)._\n\nThis page does not yet list all routes. For a full list, see the [route controller](https://github.com/TriliumNext/Notes/blob/v0.91.6/src/routes/routes.ts).",
             contact: {
