12bdf5adde01f38aa6fcdca8efebfd50994492d7
Elian Doran <contact@eliandoran.me>
2025-04-18 17:08:43 +0300

    chore(monorepo/server): remove server: prefix from scripts

--- apps/client/vitest.config.ts
+++ apps/client/vitest.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from "vitest/config";
+import { configDefaults } from "vitest/config";
 
 export default defineConfig({
     test: {
+        exclude: [
+            ...configDefaults.exclude,
+            "build/**",
+        ],
         setupFiles: ["./test/setup.ts"],
         environment: "happy-dom",
         coverage: {
             reporter: [ "text", "html" ],
-            provider: "v8"
         }
     }
 });
