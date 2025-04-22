b27ff1def5e17dd1f61ce4dae66511e86eb008d0
Elian Doran <contact@eliandoran.me>
2025-04-22 21:26:38 +0300

    chore(nx/commons): fix compilation

--- packages/commons/src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./i18n.js";
-export * from "./options_interface.js";
-export * from "./keyboard_actions_interface.js";
-export * from "./hidden_subtree.js";
-export * from "./rows.js";
-export * from "./test-utils.js"
