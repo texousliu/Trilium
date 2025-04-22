23572bd47c26952991aa512a44b9a055183e8458
Elian Doran <contact@eliandoran.me>
2025-04-22 22:06:10 +0300

    chore(monorepo/client): create empty project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,8 +4,7 @@ export default [
     ...nx.configs["flat/base"],
     ...nx.configs["flat/typescript"],
     ...nx.configs["flat/javascript"],
-    {
-      "ignores": [
+    {      "ignores": [
         "**/dist",
         "**/vite.config.*.timestamp*",
         "**/vitest.config.*.timestamp*"
