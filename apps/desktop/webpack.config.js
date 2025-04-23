4e6047f84ddf51885a8c9ffc8708ed747aa25c47
Elian Doran <contact@eliandoran.me>
2025-04-23 14:31:11 +0300

    chore(nx/desktop): copy .node file for better-sqlite3

--- apps/desktop/webpack.config.js
+++ apps/desktop/webpack.config.js
@@ -1,9 +1,12 @@
 const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const { join } = require('path');
+
+const outputDir = join(__dirname, 'dist');
 
 module.exports = {
   output: {
-    path: join(__dirname, 'dist'),
+    path: outputDir,
   },
   plugins: [
     new NxAppWebpackPlugin({
@@ -17,7 +20,18 @@ module.exports = {
       generatePackageJson: true,
       externalDependencies: [
         "electron/main"
+      ],
+      assets: [
+
       ]
+    }),
+    new CopyPlugin({
+        patterns: [
+            {
+                from: "../../node_modules/better-sqlite3/build/Release",
+                to: join(outputDir, "Release")
+            }
+        ]
     })
   ],
 };
