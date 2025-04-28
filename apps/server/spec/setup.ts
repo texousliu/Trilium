bb278d495e81aca7ce8dfc426056755aaf229cf8
Elian Doran <contact@eliandoran.me>
2025-04-28 17:35:52 +0300

    chore(test/server): fix some mocks no longer working

--- apps/server/spec/setup.ts
+++ apps/server/spec/setup.ts
@@ -1,11 +1,9 @@
 import { beforeAll } from "vitest";
-import { getResourceDir } from "../src/services/utils.js";
 import i18next from "i18next";
 import { join } from "path";
 
 beforeAll(async () => {
     // Initialize the translations manually to avoid any side effects.
-    const resourceDir = getResourceDir();
     const Backend = (await import("i18next-fs-backend")).default;
 
     // Initialize translations
@@ -14,7 +12,7 @@ beforeAll(async () => {
         fallbackLng: "en",
         ns: "server",
         backend: {
-            loadPath: join(resourceDir, "assets/translations/{{lng}}/{{ns}}.json")
+            loadPath: join(__dirname, "../src/assets/translations/{{lng}}/{{ns}}.json")
         }
     });
 });
