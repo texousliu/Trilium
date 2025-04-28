6aaacd6ca14065d0231acb0569d4dc15ecc96a61
Elian Doran <contact@eliandoran.me>
2025-04-28 18:40:24 +0300

    fix(test/server): one more failing test

--- apps/server/src/services/migration.spec.ts
+++ apps/server/src/services/migration.spec.ts
@@ -1,13 +1,16 @@
 import { describe, expect, it } from "vitest";
-import sql from "./sql.js";
-import migration from "./migration.js";
 import cls from "./cls.js";
 
 describe("Migration", () => {
     it("migrates from v214", async () => {
         await new Promise<void>((resolve) => {
             cls.init(async () => {
+                await import("../app.js");
+
+                const sql = (await (import("./sql.js"))).default;
                 sql.rebuildIntegrationTestDatabase("spec/db/document_v214.db");
+
+                const migration = (await import("./migration.js")).default;
                 await migration.migrateIfNecessary();
                 expect(sql.getValue("SELECT count(*) FROM blobs")).toBe(116);
                 resolve();
