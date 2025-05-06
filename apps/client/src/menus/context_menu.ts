2436838aae6f09973a2bbadb7e02b39dd519e3cd
SiriusXT <1160925501@qq.com>
2025-05-06 20:40:13 +0800

    Hide the content menu on mouse up

--- apps/client/src/menus/context_menu.ts
+++ apps/client/src/menus/context_menu.ts
@@ -192,6 +192,12 @@ class ContextMenu {
                         // it's important to stop the propagation especially for sub-menus, otherwise the event
                         // might be handled again by top-level menu
                         return false;
+                    })
+                    .on("mouseup", (e) =>{
+                        e.stopPropagation();
+                        // Hide the content menu on mouse up to prevent the mouse event from propagating to the elements below.
+                        this.hide();
+                        return false;
                     });
 
                 if ("enabled" in item && item.enabled !== undefined && !item.enabled) {
