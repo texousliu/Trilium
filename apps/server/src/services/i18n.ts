1d58b285dec62f7af2cf16f8b777c2de920b74b0
Elian Doran <contact@eliandoran.me>
2025-05-16 19:09:54 +0300

    fix(server): week notes broken due to misuse of transactions

--- apps/server/src/services/i18n.ts
+++ apps/server/src/services/i18n.ts
@@ -5,20 +5,43 @@ import { join } from "path";
 import { getResourceDir } from "./utils.js";
 import hidden_subtree from "./hidden_subtree.js";
 import { LOCALES, type Locale } from "@triliumnext/commons";
+import dayjs, { Dayjs } from "dayjs";
+
+const DAYJS_LOCALE_MAP: Record<string, string> = {
+    cn: "zh-cn",
+    tw: "zh-tw"
+};
+
+let dayjsLocale: string;
 
 export async function initializeTranslations() {
     const resourceDir = getResourceDir();
     const Backend = (await import("i18next-fs-backend")).default;
+    const locale = getCurrentLanguage();
 
     // Initialize translations
     await i18next.use(Backend).init({
-        lng: getCurrentLanguage(),
+        lng: locale,
         fallbackLng: "en",
         ns: "server",
         backend: {
             loadPath: join(resourceDir, "assets/translations/{{lng}}/{{ns}}.json")
         }
     });
+
+    // Initialize dayjs locale.
+    dayjsLocale = DAYJS_LOCALE_MAP[locale] ?? locale;
+    try {
+        await import(`dayjs/locale/${dayjsLocale}.js`);
+    } catch (err) {
+        console.warn(`Could not load locale ${dayjsLocale}`, err);
+    }
+}
+
+export function ordinal(date: Dayjs) {
+    return dayjs(date)
+        .locale(dayjsLocale)
+        .format("Do");
 }
 
 export function getLocales(): Locale[] {
