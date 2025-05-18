df6797d9eacb096f64c197f6033977337f671494
Elian Doran <contact@eliandoran.me>
2025-05-18 19:34:27 +0300

    fix(highlighting): highlighting not working immediately after a refresh

--- apps/client/src/widgets/type_widgets/ckeditor/config.ts
+++ apps/client/src/widgets/type_widgets/ckeditor/config.ts
@@ -104,7 +104,7 @@ export function buildConfig() {
         },
         syntaxHighlighting: {
             loadHighlightJs: async () => {
-                ensureMimeTypesForHighlighting();
+                await ensureMimeTypesForHighlighting();
                 return await import("@triliumnext/highlightjs");
             },
             mapLanguageName: getHighlightJsNameForMime,
