622d026efc9f3e5e7f5ef5b23b2c8bc9f56bfc94
Elian Doran <contact@eliandoran.me>
2025-05-26 12:23:11 +0300

    refactor(ckeditor5/codeblock): simplify copy clipboard plugin

--- packages/ckeditor5/src/plugins/copy_to_clipboard_button.ts
+++ packages/ckeditor5/src/plugins/copy_to_clipboard_button.ts
@@ -3,18 +3,11 @@ import copyIcon from "../icons/copy.svg?raw";
 
 export default class CopyToClipboardButton extends Plugin {
 
-    static get requires() {
-        return [ CopyToClipboardEditing, CopyToClipboardUI ];
-    }
-
-}
-
-export class CopyToClipboardUI extends Plugin {
-
     public init() {
         const editor = this.editor;
-        const componentFactory = editor.ui.componentFactory;
+        editor.commands.add("copyToClipboard", new CopyToClipboardCommand(this.editor));
 
+        const componentFactory = editor.ui.componentFactory;
         componentFactory.add("copyToClipboard", locale => {
             const button = new ButtonView(locale);
             button.set({
@@ -28,14 +21,6 @@ export class CopyToClipboardUI extends Plugin {
 
             return button;
         });
-    }
-
-}
-
-export class CopyToClipboardEditing extends Plugin {
-
-    public init() {
-        this.editor.commands.add("copyToClipboard", new CopyToClipboardCommand(this.editor));
     }
 
 }
