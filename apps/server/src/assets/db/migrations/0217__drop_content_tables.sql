8994f537dca736ca6831c114e4db592f6c007b12
Elian Doran <contact@eliandoran.me>
2025-05-30 21:32:35 +0300

    fix(migration): use bundleable migrations
    This fixes TypeScript migrations, which were not being run correctly
    even in previous versions.
    
    On the ESBuild version, neither SQL migrations worked due to the fact
    that they were not being bundled.

--- apps/server/src/assets/db/migrations/0217__drop_content_tables.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE note_contents;
-DROP TABLE note_revision_contents;
-
-DELETE FROM entity_changes WHERE entityName IN ('note_contents', 'note_revision_contents');
--- apps/server/src/assets/db/migrations/0222__rename_openTabs_to_openNoteContexts.sql
+++
@@ -1,2 +0,0 @@
-UPDATE options SET name = 'openNoteContexts' WHERE name = 'openTabs';
-UPDATE entity_changes SET entityId = 'openNoteContexts' WHERE entityName = 'options' AND entityId = 'openTabs';
