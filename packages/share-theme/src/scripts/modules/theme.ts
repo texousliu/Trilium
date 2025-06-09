02fe7c97cac01a64caab0d0879d6ea9cae43b58f
Elian Doran <contact@eliandoran.me>
2025-06-09 19:38:44 +0300

    feat(share): automatic light/dark mode

--- packages/share-theme/src/scripts/modules/theme.ts
+++ packages/share-theme/src/scripts/modules/theme.ts
@@ -1,32 +1,26 @@
-const preference = localStorage.getItem("theme");
-if (preference) {
-    if (preference === "dark") {
-        document.body.classList.add("theme-dark");
-        document.body.classList.remove("theme-light");
-    }
-    else {
-        document.body.classList.remove("theme-dark");
-        document.body.classList.add("theme-light");
-    }
+const prefersDark = localStorage.getItem("theme") === "dark" || (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+if (prefersDark) {
+    document.body.classList.add("theme-dark");
+    document.body.classList.remove("theme-light");
+} else {
+    document.body.classList.remove("theme-dark");
+    document.body.classList.add("theme-light");
 }
 
 export default function setupThemeSelector() {
     const themeSwitch: HTMLInputElement = document.querySelector(".theme-selection input")!;
-    
-    if (preference) {
-        const themeSelection: HTMLDivElement = document.querySelector(".theme-selection")!;
-        themeSelection.classList.add("no-transition");
-        themeSwitch.checked = preference === "dark";
-        setTimeout(() => themeSelection.classList.remove("no-transition"), 400);
-    }
+
+    const themeSelection: HTMLDivElement = document.querySelector(".theme-selection")!;
+    themeSelection.classList.add("no-transition");
+    themeSwitch.checked = prefersDark;
+    setTimeout(() => themeSelection.classList.remove("no-transition"), 400);
 
     themeSwitch?.addEventListener("change", () => {
         if (themeSwitch.checked) {
             document.body.classList.add("theme-dark");
             document.body.classList.remove("theme-light");
             localStorage.setItem("theme", "dark");
-        }
-        else {
+        } else {
             document.body.classList.remove("theme-dark");
             document.body.classList.add("theme-light");
             localStorage.setItem("theme", "light");
