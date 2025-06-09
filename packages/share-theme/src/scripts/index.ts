ea015bc2cf4a8881b3b693dccf7aca27d1e2ed9f
Elian Doran <contact@eliandoran.me>
2025-06-09 19:55:21 +0300

    refactor(share): remove no longer necessary highlight module

--- packages/share-theme/src/scripts/index.ts
+++ packages/share-theme/src/scripts/index.ts
@@ -1,10 +1,8 @@
-import highlight from "./modules/highlight";
 import setupToC from "./modules/toc";
 import setupExpanders from "./modules/expanders";
 import setupMobileMenu from "./modules/mobile";
 import setupSearch from "./modules/search";
 import setupThemeSelector from "./modules/theme";
-
 
 function $try<T extends (...a: unknown[]) => unknown>(func: T, ...args: Parameters<T>) {
     try {
@@ -17,7 +15,6 @@ function $try<T extends (...a: unknown[]) => unknown>(func: T, ...args: Paramete
 
 $try(setupThemeSelector);
 $try(setupToC);
-$try(highlight);
 $try(setupExpanders);
 $try(setupMobileMenu);
 $try(setupSearch);
