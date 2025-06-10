ee9267775e9adc697d8ada8e01e8676559a78a2d
SiriusXT <1160925501@qq.com>
2025-06-10 17:38:05 +0800

    vitest(link): add Vitest test cases for parseNavigationStateFromUrl

--- apps/client/src/services/link.spec.ts
+++ apps/client/src/services/link.spec.ts
@@ -22,6 +22,11 @@ describe("Link", () => {
         expect(output).toMatchObject({ notePath: "root/WWaBNf3SSA1b/mQ2tIzLVFKHL", noteId: "mQ2tIzLVFKHL" });
     });
 
+    it("parses notePath with extraWindow", () => {
+        const output = parseNavigationStateFromUrl(`127.0.0.1:8080/?extraWindow=1#root/QZGqKB7wVZF8?ntxId=0XPvXG`);
+        expect(output).toMatchObject({ notePath: "root/QZGqKB7wVZF8", noteId: "QZGqKB7wVZF8" });
+    });
+
     it("ignores external URL with internal hash anchor", () => {
         const output = parseNavigationStateFromUrl(`https://en.wikipedia.org/wiki/Bearded_Collie#Health`);
         expect(output).toMatchObject({});
