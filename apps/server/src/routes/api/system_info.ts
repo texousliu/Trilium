f6bba436f4c46ccbddb811f46864571fcb473e8c
Elian Doran <contact@eliandoran.me>
2025-06-12 21:46:27 +0300

    Revert "fix(client): also move the logic from the server to the client lol"
    This reverts commit e401c8c930c8160f5acd83cfff3b97aef3ca152a.

---
+++ apps/server/src/routes/api/system_info.ts
@@ -0,0 +1,12 @@
+import { isRunningUnderRosetta2 } from "../../services/utils.js";
+import type { Request, Response } from "express";
+
+function rosettaCheck(req: Request, res: Response) {
+    res.json({
+        isRunningUnderRosetta2: isRunningUnderRosetta2()
+    });
+}
+
+export default {
+    rosettaCheck
+};
