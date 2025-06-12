db3c008c07bff7f2f103512fbb780b68c3e24c2f
Elian Doran <contact@eliandoran.me>
2025-06-12 22:07:50 +0300

    fix(server): headers sent twice

--- apps/server/src/routes/api/system_info.ts
+++ apps/server/src/routes/api/system_info.ts
@@ -2,9 +2,9 @@ import { isRunningUnderRosetta2 } from "../../services/utils.js";
 import type { Request, Response } from "express";
 
 function rosettaCheck(req: Request, res: Response) {
-    res.json({
+    return {
         isRunningUnderRosetta2: isRunningUnderRosetta2()
-    });
+    }
 }
 
 export default {
