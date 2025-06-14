1848d5dc967c72a18a9ab37666818575228c606b
Elian Doran <contact@eliandoran.me>
2025-06-14 23:54:19 +0300

    chore(types): type missing in the CI

--- apps/website/src/hooks.ts
+++ apps/website/src/hooks.ts
@@ -1,3 +1,6 @@
 import { deLocalizeUrl } from '$lib/paraglide/runtime';
 
-export const reroute = (request) => deLocalizeUrl(request.url).pathname;
+export const reroute = (request: {
+    url: URL;
+    fetch: typeof fetch;
+}) => deLocalizeUrl(request.url).pathname;
