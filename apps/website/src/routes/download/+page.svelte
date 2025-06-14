630aade97ab8ff9158922cdfdcb8f56fbc019f66
Elian Doran <contact@eliandoran.me>
2025-06-14 13:18:34 +0300

    feat(website): start a download section

---
+++ apps/website/src/routes/download/+page.svelte
@@ -0,0 +1,63 @@
+<section class="mt-20 max-w-6xl mx-auto px-4">
+    <h2 class="text-3xl font-bold text-center mb-12">Desktop application</h2>
+
+    <div class="grid md:grid-cols-3 gap-10">
+        <div class="bg-white rounded-xl shadow overflow-hidden">
+            <div class="p-6">
+                <h3 class="text-xl font-semibold mb-2">Windows</h3>
+                
+                <div class="flex flex-wrap gap-y-2">
+                    <a class="py-2 px-5 bg-violet-600 text-white font-semibold rounded-full shadow-md hover:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-400 focus:ring-opacity-75 grow" href="#">
+                        Installer (.exe)
+                        <span class="text-sm text-gray-300 ml-2">Recommended</span>
+                    </a>
+
+                    <a class="py-2 px-5 border-1 border-gray-500 text-gray-500 font-semibold rounded-full shadow-md grow" href="#">
+                        Portable (.zip)
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="bg-white rounded-xl shadow overflow-hidden">
+            <div class="p-6">
+                <h3 class="text-xl font-semibold mb-2">Linux</h3>
+
+                <div class="flex flex-wrap gap-y-2">                    
+                    <a class="py-2 px-5 border-1 border-gray-500 text-gray-500 font-semibold rounded-full shadow-md grow" href="#">
+                        Debian-based distributions (.deb)
+                    </a>
+
+                    <a class="py-2 px-5 border-1 border-gray-500 text-gray-500 font-semibold rounded-full shadow-md grow" href="#">
+                        Red Hat-based distributions (.rpm)
+                    </a>
+
+                    <a class="py-2 px-5 border-1 border-gray-500 text-gray-500 font-semibold rounded-full shadow-md grow" href="#">
+                        Flatpak (.flatpak)
+                    </a>
+
+                    <a class="py-2 px-5 border-1 border-gray-500 text-gray-500 font-semibold rounded-full shadow-md grow" href="#">
+                        Portable (.zip)
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="bg-white rounded-xl shadow overflow-hidden">
+            <div class="p-6">
+                <h3 class="text-xl font-semibold mb-2">macOS</h3>
+
+                <div class="flex flex-wrap gap-y-2">
+                    <a class="py-2 px-5 bg-violet-600 text-white font-semibold rounded-full shadow-md hover:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-400 focus:ring-opacity-75 grow" href="#">
+                        Disk image (.dmg)
+                        <span class="text-sm text-gray-300 ml-2">Recommended</span>
+                    </a>
+
+                    <a class="py-2 px-5 border-1 border-gray-500 text-gray-500 font-semibold rounded-full shadow-md grow" href="#">
+                        Portable (.zip)
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
