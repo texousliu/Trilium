09dd8f9828f6d9e512077086a63a177b3ad54994
Elian Doran <contact@eliandoran.me>
2025-06-14 11:48:17 +0300

    feat(website): slight improvements to header

--- apps/website/src/routes/header.svelte
+++ apps/website/src/routes/header.svelte
@@ -1,8 +1,9 @@
-<header class="header">
-    <div class="container mx-auto flex items-center">
-        <div class="header-logo">
-            Logo goes here.
-        </div>
+<header class="header bg-white sticky top-0">
+    <div class="container mx-auto flex items-center py-5">
+        <a href="/" class="flex items-center gap-x-2 w-100">
+            <img src="icon-color.svg" alt="Trilium Notes Logo" class="w-10 h-10">
+            <span class="text-xl">Trilium Notes</span>
+        </a>
 
         <div class="group w-full">
             <nav class="header-nav">
