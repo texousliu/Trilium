9eda9b139963398195ef5a462b65205ca6322874
Elian Doran <contact@eliandoran.me>
2025-06-14 11:20:01 +0300

    feat(website): basic hero section

--- apps/website/src/routes/+page.svelte
+++ apps/website/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKitt</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section class="hero">
+    <div class="container pt-24 pb-24 mx-auto">
+        <h2 class="text-3xl font-bold mb-4">Trilium Notes</h2>
+        <p class="text-lg mb-6">Build your personal knowledge base with Trilium Notes</p>
+        
+        <div class="flex items-center gap-6">
+            <a href="#" class="download-button button py-2 px-5 bg-violet-500 text-white font-semibold rounded-full shadow-md hover:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-400 focus:ring-opacity-75">Download</a>
+            <a href="#" class="font-medium">More platforms</a>
+        </div>
+    </div>
+</section>
