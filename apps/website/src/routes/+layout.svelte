e66e70f7f253752e3d3b1235ae9b390d59d598c2
Elian Doran <contact@eliandoran.me>
2025-06-14 11:13:40 +0300

    feat(website): very simple header

--- apps/website/src/routes/+layout.svelte
+++ apps/website/src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import '../app.css';
+    import Header from './header.svelte';
 	
 	let { children } = $props();
 </script>
+
+<Header />
 
 {@render children()}
---
+++ apps/website/src/routes/header.svelte
@@ -0,0 +1,18 @@
+<header class="header">
+    <div class="container mx-auto flex items-center">
+        <div class="header-logo">
+            Logo goes here.
+        </div>
+
+        <div class="group w-full">
+            <nav class="header-nav">
+                <ul class="flex items-center justify-end gap-4">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/services">Services</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
