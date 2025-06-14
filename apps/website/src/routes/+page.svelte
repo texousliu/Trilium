7dd6c1a6ccef5eaad93db9df13db9189621b0030
Elian Doran <contact@eliandoran.me>
2025-06-14 11:42:02 +0300

    feat(website): basic screenshot

--- apps/website/src/routes/+page.svelte
+++ apps/website/src/routes/+page.svelte
@@ -6,6 +6,10 @@
         <div class="flex items-center gap-6">
             <a href="#" class="download-button button py-2 px-5 bg-violet-500 text-white font-semibold rounded-full shadow-md hover:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-400 focus:ring-opacity-75">Download</a>
             <a href="#" class="font-medium">More platforms</a>
-        </div>
+        </div>        
+    </div>
+
+    <div class="container mx-auto">
+        <img src="screenshots/desktop-win.png" alt="Screenshot of the app on desktop Windows" />
     </div>
 </section>
