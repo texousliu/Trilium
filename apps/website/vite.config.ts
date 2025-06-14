296c95e80046eeba212cbed35b4e297fd4d3262f
Elian Doran <contact@eliandoran.me>
2025-06-14 22:40:45 +0300

    chore(nx): configure for SvelteKit

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -3,13 +3,22 @@ import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit(),
-		paraglideVitePlugin({
-			project: './project.inlang',
-			outdir: './src/lib/paraglide'
-		})
-	]
-});
+export default () => {
+    // See https://github.com/nrwl/nx/issues/28978.
+    const cwd = process.cwd();
+    process.chdir(__dirname); // Temporarily change the working directory
+
+    const config = defineConfig({
+        plugins: [
+            tailwindcss(),
+            sveltekit(),
+            paraglideVitePlugin({
+                project: './project.inlang',
+                outdir: './src/lib/paraglide'
+            })
+        ]
+    });
+
+    process.chdir(cwd); // Restore the original working directory
+    return config;
+};
