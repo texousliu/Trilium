dcccb5ad30c8054659852a152c4a8c96a57c3109
Elian Doran <contact@eliandoran.me>
2025-06-17 19:07:05 +0300

    feat(builtin_templates): add icon for text snippets

--- apps/server/src/services/hidden_subtree_templates.ts
+++ apps/server/src/services/hidden_subtree_templates.ts
@@ -10,6 +10,7 @@ export default function buildHiddenSubtreeTemplates() {
                 id: "_template_text_snippet",
                 type: "text",
                 title: "Text Snippet",
+                icon: "bx bx-align-left",
                 attributes: [
                     {
                         name: "template",
