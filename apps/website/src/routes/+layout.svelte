d1583ca091b279ff4c20b6fae2ff0a814e6efc94
FliegendeWurst <arne.keller@posteo.de>
2025-06-17 17:01:13 +0200

    fix(website): normal link styling

--- apps/website/src/routes/+layout.svelte
+++ apps/website/src/routes/+layout.svelte
@@ -7,7 +7,9 @@
 
 <Header />
 
+<main>
 {@render children()}
+</main>
 
 <footer class="container mx-auto bg-white mt-2 py-6 text-sm text-center text-gray-500">
     &copy; 2024-2025 <a href="https://github.com/eliandoran" class="text-blue-500 hover:underline">Elian Doran</a> and the <a href="https://github.com/TriliumNext/Notes/graphs/contributors" class="text-blue-500 hover:underline">team</a>. <br/>    &copy; 2017-2024 <a href="https://github.com/zadam" class="text-blue-500 hover:underline">Adam Zivner</a>.
