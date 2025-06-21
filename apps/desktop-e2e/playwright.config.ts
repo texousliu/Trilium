ff917e93c98a2ed97ac9d73f65ae6f58d60d1fa9
Elian Doran <contact@eliandoran.me>
2025-06-21 14:28:57 +0300

    e2e(desktop): fix process leak

--- apps/desktop-e2e/playwright.config.ts
+++ apps/desktop-e2e/playwright.config.ts
@@ -1,18 +1,14 @@
 import { defineConfig, devices } from '@playwright/test';
-import { nxE2EPreset } from '@nx/playwright/preset';
 
 require('dotenv').config({
     path: __dirname + "/" + ".env"
 });
 
-// For CI, you may want to set BASE_URL to the deployed application.
-const port = process.env['TRILIUM_PORT'];
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  ...nxE2EPreset(__filename, { testDir: './src' }),
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
