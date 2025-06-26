fe3350f39ffc3e90640e848d79827e0e6bcc6e1a
Elian Doran <contact@eliandoran.me>
2025-06-26 20:13:01 +0300

    feat(server/script): enable a few dayjs plugins (closes #6080)

--- apps/server/src/services/script.spec.ts
+++ apps/server/src/services/script.spec.ts
@@ -60,16 +60,54 @@ describe("Script", () => {
     });
 
     describe("dayjs", () => {
+        const scriptNote = note("dayjs", {
+            type: "code",
+            mime: "application/javascript;env=backend",
+        });
+
         it("dayjs is available", () => {
             cls.init(() => {
-                const scriptNote = note("dayjs", {
-                    type: "code",
-                    mime: "application/javascript;env=backend",
-                });
                 const bundle = getScriptBundle(scriptNote.note, true, "backend", [], `return api.dayjs().format("YYYY-MM-DD");`);
                 expect(bundle).toBeDefined();
                 const result = executeBundle(bundle!);
                 expect(result).toMatch(/^\d{4}-\d{2}-\d{2}$/);
+            });
+        });
+
+        it("dayjs is-same-or-before", () => {
+            cls.init(() => {
+                const bundle = getScriptBundle(scriptNote.note, true, "backend", [], `return api.dayjs("2023-10-01").isSameOrBefore(api.dayjs("2023-10-02"));`);
+                expect(bundle).toBeDefined();
+                const result = executeBundle(bundle!);
+                expect(result).toBe(true);
+            });
+        });
+
+        it("dayjs is-same-or-after", () => {
+            cls.init(() => {
+                const bundle = getScriptBundle(scriptNote.note, true, "backend", [], `return api.dayjs("2023-10-02").isSameOrAfter(api.dayjs("2023-10-01"));`);
+                expect(bundle).toBeDefined();
+                const result = executeBundle(bundle!);
+                expect(result).toBe(true);
+            });
+        });
+
+        it("dayjs is-between", () => {
+            cls.init(() => {
+                const bundle = getScriptBundle(scriptNote.note, true, "backend", [], `return api.dayjs("2023-10-02").isBetween(api.dayjs("2023-10-01"), api.dayjs("2023-10-03"));`);
+                expect(bundle).toBeDefined();
+                const result = executeBundle(bundle!);
+                expect(result).toBe(true);
+            });
+        });
+
+        // advanced format
+        it("dayjs advanced format", () => {
+            cls.init(() => {
+                const bundle = getScriptBundle(scriptNote.note, true, "backend", [], `return api.dayjs("2023-10-01").format("Q");`);
+                expect(bundle).toBeDefined();
+                const result = executeBundle(bundle!);
+                expect(result).not.toBe("Q");
             });
         });
     });
