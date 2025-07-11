72a779e695fc462926e3521c90169b49078e35d5
perf3ct <jonfuller2012@gmail.com>
2025-07-11 22:35:05 +0000

    idk

--- apps/desktop/electron-forge/forge.config.ts
+++ apps/desktop/electron-forge/forge.config.ts
@@ -38,7 +38,7 @@ const config: ForgeConfig = {
         name: PRODUCT_NAME,
         overwrite: true,
         asar: {
-            unpack: "{**/node_modules/swagger-ui-dist/**,**/node_modules/swagger-ui-express/**,**/assets/*.yaml,**/assets/*.json}"
+            unpackDir: "{node_modules/swagger-ui-dist,node_modules/swagger-ui-express,assets}"
         },
         icon: path.join(APP_ICON_PATH, "icon"),
         ...macosSignConfiguration,
