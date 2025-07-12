6c157675d701646d70569e13bb9f14f8f172491d
Elian Doran <contact@eliandoran.me>
2025-07-13 00:44:44 +0300

    feat(views/table): open in new tab/quick edit

--- apps/client/src/widgets/view_widgets/table_view/context_menu.ts
+++ apps/client/src/widgets/view_widgets/table_view/context_menu.ts
@@ -3,6 +3,7 @@ import contextMenu from "../../../menus/context_menu.js";
 import { TableData } from "./rows.js";
 import branches from "../../../services/branches.js";
 import { t } from "../../../services/i18n.js";
+import link_context_menu from "../../../menus/link_context_menu.js";
 
 export function setupContextMenu(tabulator: Tabulator) {
     tabulator.on("rowContext", showRowContextMenu);
@@ -13,13 +14,17 @@ export function showRowContextMenu(_e: UIEvent, row: RowComponent) {
     const rowData = row.getData() as TableData;
     contextMenu.show({
         items: [
+            ...link_context_menu.getItems(),
+            { title: "----" },
             {
                 title: t("table_context_menu.delete_row"),
                 uiIcon: "bx bx-trash",
                 handler: () => branches.deleteNotes([ rowData.branchId ], false, false)
             }
         ],
-        selectMenuItemHandler: () => {},
+        selectMenuItemHandler: ({ command }) => {
+            link_context_menu.handleLinkContextMenuItem(command, rowData.noteId);
+        },
         x: e.pageX,
         y: e.pageY
     });
