c0a55fec60b80b5081ece2ea5354ef7b0ca4a044
perf3ct <jonfuller2012@gmail.com>
2025-07-12 00:33:25 +0000

    idk

--- apps/desktop/electron-forge/forge.config.ts
+++ apps/desktop/electron-forge/forge.config.ts
@@ -37,7 +37,9 @@ const config: ForgeConfig = {
         executableName: EXECUTABLE_NAME,
         name: PRODUCT_NAME,
         overwrite: true,
-        asar: true,
+        asar: {
+            unpack: "**/.pnpm/**/swagger-ui-dist/**"
+        },
         icon: path.join(APP_ICON_PATH, "icon"),
         ...macosSignConfiguration,
         windowsSign: windowsSignConfiguration,
