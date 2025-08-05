523c7ac273fd2415de099a6353c706f97b19e24e
Elian Doran <contact@eliandoran.me>
2025-08-05 22:11:16 +0300

    chore(react/dialogs): improve display of description

--- apps/client/src/widgets/react/FormGroup.tsx
+++ apps/client/src/widgets/react/FormGroup.tsx
@@ -13,11 +13,10 @@ export default function FormGroup({ label, title, className, children, descripti
     return (
         <div className={`form-group ${className}`} title={title}>
             <label style={{ width: "100%" }} ref={labelRef}>
-                {label}
-                {children}
+                {label} {children}
             </label>
 
-            {description && <small className="form-text text-muted">{description}</small>}
+            {description && <small className="form-text">{description}</small>}
         </div>
     );
 }
