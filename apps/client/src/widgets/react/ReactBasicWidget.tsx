861374bb875a044dafe44556f25de3c02481c59a
Elian Doran <contact@eliandoran.me>
2025-08-10 14:06:14 +0300

    fix(react/dialogs): bulk actions not working in search notes

--- apps/client/src/widgets/react/ReactBasicWidget.tsx
+++ apps/client/src/widgets/react/ReactBasicWidget.tsx
@@ -8,14 +8,25 @@ export default abstract class ReactBasicWidget extends BasicWidget {
 
     abstract get component(): JSX.Element;
 
-    doRender() {
-        const renderContainer = new DocumentFragment();
-        render((
-            <ParentComponent.Provider value={this}>
-                {this.component}
-            </ParentComponent.Provider>
-        ), renderContainer);
-        this.$widget = $(renderContainer.firstChild as HTMLElement);
+    doRender() {        
+        this.$widget = renderReactWidget(this, this.component);
     }
 
+}
+
+/**
+ * Renders a React component and returns the corresponding DOM element wrapped in JQuery.
+ * 
+ * @param parentComponent the parent Trilium component for the component to be able to handle events.
+ * @param el the JSX element to render.
+ * @returns the rendered wrapped DOM element.
+ */
+export function renderReactWidget(parentComponent: Component, el: JSX.Element) {
+    const renderContainer = new DocumentFragment();
+    render((
+        <ParentComponent.Provider value={parentComponent}>
+            {el}
+        </ParentComponent.Provider>
+    ), renderContainer);
+    return $(renderContainer.firstChild as HTMLElement);
 }
