9a4fdcaef2bad892ee301bd93ec4de395262656d
Elian Doran <contact@eliandoran.me>
2025-08-21 10:34:30 +0300

    chore(react/note_title): bring back styles

--- apps/client/src/widgets/note_title.tsx
+++ apps/client/src/widgets/note_title.tsx
@@ -1,14 +1,17 @@
 import { useEffect, useRef, useState } from "preact/hooks";
 import { t } from "../services/i18n";
 import FormTextBox from "./react/FormTextBox";
-import { useNoteContext, useSpacedUpdate } from "./react/hooks";
+import { useNoteContext, useSpacedUpdate, useTriliumEventBeta } from "./react/hooks";
 import protected_session_holder from "../services/protected_session_holder";
 import server from "../services/server";
+import "./note_title.css";
 
 export default function NoteTitleWidget() {
     const { note, noteId, componentId } = useNoteContext();
     const [ title, setTitle ] = useState(note?.title);
-    useEffect(() => setTitle(note?.title), [ note?.title ]);
+    const [ isProtected, setProtected ] = useState(note?.isProtected);
+    useEffect(() => setTitle(note?.title), [ note?.noteId ]);
+    useEffect(() => setProtected(note?.isProtected), [ note?.isProtected ]);
 
     const spacedUpdate = useSpacedUpdate(async () => {
         if (!note) {
@@ -18,19 +21,28 @@ export default function NoteTitleWidget() {
         await server.put<void>(`notes/${noteId}/title`, { title: title }, componentId);
     });    
 
+    useTriliumEventBeta("entitiesReloaded", ({ loadResults }) => {        
+        if (loadResults.isNoteReloaded(noteId) && note) {
+            setProtected(note.isProtected);
+        }
+        if (loadResults.isNoteReloaded(noteId, componentId)) {
+            setTitle(note?.title);
+        }
+    });
+
     return (
-        <>
+        <div className="note-title-widget">
             <FormTextBox
                 autocomplete="off"
                 currentValue={title}
                 placeholder={t("note_title.placeholder")}
-                className="note-title"
+                className={`note-title ${isProtected ? "protected" : ""}`}
                 tabIndex={100}
                 onChange={(newValue) => {
                     setTitle(newValue);
                     spacedUpdate.scheduleUpdate();
                 }}
             />
-        </>
+        </div>
     );
 }
