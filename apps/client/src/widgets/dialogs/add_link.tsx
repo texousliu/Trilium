24fd898f0d32cdc279b5c19d5086e4aa10270ca3
Elian Doran <contact@eliandoran.me>
2025-08-25 20:48:15 +0300

    fix(add_link): inserting link to selection not working properly (closes #6776)

--- apps/client/src/widgets/dialogs/add_link.tsx
+++ apps/client/src/widgets/dialogs/add_link.tsx
@@ -30,6 +30,14 @@ function AddLinkDialogComponent() {
         setShown(true);
     });
 
+    useEffect(() => {
+        if (hasSelection) {
+            setLinkType("hyper-link");
+        } else {
+            setLinkType("reference-link");
+        }
+    }, [ hasSelection ])
+
     async function setDefaultLinkTitle(noteId: string) {
         const noteTitle = await tree.getNoteTitle(noteId);
         setLinkTitle(noteTitle);
@@ -90,6 +98,7 @@ function AddLinkDialogComponent() {
     }
 
     const autocompleteRef = useRef<HTMLInputElement>(null);
+    console.log("Add link ", linkType, hasSelection);
 
     return (
         <Modal
