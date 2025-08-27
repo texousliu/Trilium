1c7dfa6c91a371c875c794a84164c7f7dadd732f
Elian Doran <contact@eliandoran.me>
2025-08-27 13:15:38 +0300

    chore(react/ribbon): fix activation of add new label to all tabs

--- apps/client/src/widgets/ribbon/OwnedAttributesTab.tsx
+++ apps/client/src/widgets/ribbon/OwnedAttributesTab.tsx
@@ -2,13 +2,17 @@ import { useTriliumEvents } from "../react/hooks";
 import AttributeEditor from "./components/AttributeEditor";
 import { TabContext } from "./ribbon-interface";
 
-export default function OwnedAttributesTab({ note, hidden, activate, ...restProps }: TabContext) {
-    useTriliumEvents([ "addNewLabel", "addNewRelation" ], activate);
+export default function OwnedAttributesTab({ note, hidden, activate, ntxId, ...restProps }: TabContext) {
+    useTriliumEvents([ "addNewLabel", "addNewRelation" ], ({ ntxId: eventNtxId }) => {
+        if (ntxId === eventNtxId) {
+            activate();
+        }
+    });
 
     return (
         <div className="attribute-list">
             { note && (
-                <AttributeEditor note={note} {...restProps} hidden={hidden} />
+                <AttributeEditor ntxId={ntxId} note={note} {...restProps} hidden={hidden} />
             )}
         </div>
     )
