28605f26874d6b7daec3099662aedf480039ec2e
Elian Doran <contact@eliandoran.me>
2025-08-27 23:20:19 +0300

    feat(react/floating_buttons): fancy title + keyboard shortcut

--- apps/client/src/widgets/react/ActionButton.tsx
+++ apps/client/src/widgets/react/ActionButton.tsx
@@ -1,18 +1,36 @@
+import { useEffect, useRef, useState } from "preact/hooks";
 import { CommandNames } from "../../components/app_context";
+import { useStaticTooltip } from "./hooks";
+import keyboard_actions from "../../services/keyboard_actions";
 
 interface ActionButtonProps {
     text: string;
-    titlePosition?: "bottom"; // TODO: Use it
+    titlePosition?: "bottom" | "left"; // TODO: Use it
     icon: string;
     className?: string;
     onClick?: (e: MouseEvent) => void;
     triggerCommand?: CommandNames;
 }
 
-export default function ActionButton({ text, icon, className, onClick, triggerCommand }: ActionButtonProps) {
+export default function ActionButton({ text, icon, className, onClick, triggerCommand, titlePosition }: ActionButtonProps) {
+    const buttonRef = useRef<HTMLButtonElement>(null);
+    const [ keyboardShortcut, setKeyboardShortcut ] = useState<string[]>();
+    
+    useStaticTooltip(buttonRef, {
+        title: keyboardShortcut?.length ? `${text} (${keyboardShortcut?.join(",")})` : text,
+        placement: titlePosition ?? "bottom",
+        fallbackPlacements: [ titlePosition ?? "bottom" ]
+    });
+    
+    useEffect(() => {
+        if (triggerCommand) {
+            keyboard_actions.getAction(triggerCommand).then(action => setKeyboardShortcut(action?.effectiveShortcuts));
+        }
+    }, [triggerCommand]);
+
     return <button
+        ref={buttonRef}
         class={`icon-action ${icon} ${className ?? ""}`}
-        title={text}
         onClick={onClick}        
         data-trigger-command={triggerCommand}
     />;
