46cb869237b958692f957f1dfc0bcb1795887e49
Elian Doran <contact@eliandoran.me>
2025-08-31 19:21:36 +0300

    chore(dx/server): client paths not correct

--- apps/server/src/services/asset_path.ts
+++ apps/server/src/services/asset_path.ts
@@ -1,6 +1,7 @@
 import packageJson from "../../package.json" with { type: "json" };
+import { isDev } from "./utils";
 
 export const assetUrlFragment = `assets/v${packageJson.version}`;
-const assetPath = assetUrlFragment;
+const assetPath = isDev ? assetUrlFragment + "/src" : assetUrlFragment;
 
 export default assetPath;
