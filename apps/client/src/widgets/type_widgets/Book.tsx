db7cda3fe633bf309fcdb4e5c3ae57de98204020
Elian Doran <contact@eliandoran.me>
2025-09-19 19:06:07 +0300

    chore(react/type_widget): have book react to reloaded children

--- apps/client/src/widgets/type_widgets/Book.tsx
+++ apps/client/src/widgets/type_widgets/Book.tsx
@@ -1,15 +1,27 @@
 import { t } from "../../services/i18n";
 import Alert from "../react/Alert";
-import { useNoteLabel } from "../react/hooks";
+import { useNoteLabel, useTriliumEvent } from "../react/hooks";
 import RawHtml from "../react/RawHtml";
 import { TypeWidgetProps } from "./type_widget";
 import "./Book.css";
+import { useEffect, useState } from "preact/hooks";
 
 const VIEW_TYPES = [ "list", "grid" ];
 
 export default function Book({ note }: TypeWidgetProps) {
     const [ viewType ] = useNoteLabel(note, "viewType");
-    const shouldDisplayNoChildrenWarning = !note.hasChildren() && VIEW_TYPES.includes(viewType ?? "");
+    const [ shouldDisplayNoChildrenWarning, setShouldDisplayNoChildrenWarning ] = useState(false);
+
+    function refresh() {
+        setShouldDisplayNoChildrenWarning(!note.hasChildren() && VIEW_TYPES.includes(viewType ?? ""));
+    }
+
+    useEffect(refresh, [ note ]);
+    useTriliumEvent("entitiesReloaded", ({ loadResults }) => {
+        if (loadResults.getBranchRows().some(branchRow => branchRow.parentNoteId === note.noteId)) {
+            refresh();
+        }
+    });
 
     return (
         <div className="note-detail-book note-detail-printable">
