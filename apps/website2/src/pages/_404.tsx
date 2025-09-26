428abb4591d493529610df32d6cdee975de5eb90
Elian Doran <contact@eliandoran.me>
2025-09-27 01:20:59 +0300

    feat(website): improve 404 page

--- apps/website2/src/pages/_404.tsx
+++ apps/website2/src/pages/_404.tsx
@@ -1,9 +1,10 @@
 import Section from "../components/Section";
+import "./_404.css";
 
 export function NotFound() {
 	return (
-		<Section title="404: Not Found">
-			<p>It's gone :(</p>
+		<Section title="404: Not Found" className="section-404">
+            The page you were looking for could not be found. Maybe it was deleted or the URL is incorrect.
 		</Section>
 	);
 }
