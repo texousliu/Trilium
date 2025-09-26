2df646f80cfe4d65d9904b62475ccf61d8198725
Elian Doran <contact@eliandoran.me>
2025-09-26 23:35:28 +0300

    refactor(website): move back into website2

--- apps/website2/vite.config.ts
+++ apps/website2/vite.config.ts
@@ -1,6 +1,17 @@
-import { join } from "path";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
 
-export default defineConfig(() => ({
-    root: join(__dirname, "src")
-}));
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		preact({
+			prerender: {
+				enabled: true,
+				renderTarget: '#app',
+				additionalPrerenderRoutes: ['/404'],
+				previewMiddlewareEnabled: true,
+				previewMiddlewareFallback: '/404',
+			},
+		}),
+	],
+});
