35853ff9884ce942a1107c166e3e63308dec54ea
Elian Doran <contact@eliandoran.me>
2025-09-27 17:04:31 +0300

    fix(website): logo not working in subfolder

--- apps/website/src/components/Header.tsx
+++ apps/website/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import "./Header.css";
 import { useLocation } from 'preact-iso';
 import DownloadButton from './DownloadButton';
+import logoPath from "../assets/icon-color.svg";
 
 interface HeaderLink {
     url: string;
@@ -20,7 +21,7 @@ export function Header() {
 		<header>
             <div class="content-wrapper">
                 <a class="banner" href="/">
-                    <img src="./src/assets/icon-color.svg" width="300" height="300" />&nbsp;<span>Trilium Notes</span>
+                    <img src={logoPath} width="300" height="300" />&nbsp;<span>Trilium Notes</span>
                 </a>
 
                 <nav>
