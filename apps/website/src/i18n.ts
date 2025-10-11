2c71e995f20e0c61c62504ea2c274cf329b7403c
Elian Doran <contact@eliandoran.me>
2025-10-11 20:46:29 +0300

    feat(website/i18n): set up

---
+++ apps/website/src/i18n.ts
@@ -0,0 +1,16 @@
+import { default as i18next } from "i18next";
+import HttpApi from 'i18next-http-backend';
+
+console.log("INIT I18NEXT");
+i18next.use(HttpApi)
+await i18next.init({
+    debug: true,
+    lng: "en",
+    fallbackLng: "en",
+    backend: {
+        loadPath: "/translations/{{lng}}/{{ns}}.json",
+    },
+    returnEmptyString: false
+});
+
+export const t = i18next.t;
