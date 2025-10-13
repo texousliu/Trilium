d978c38b808a84fd62d8aab3800c56ddce3c8189
Elian Doran <contact@eliandoran.me>
2025-10-13 19:05:05 +0300

    feat(website/i18n): translate 404

--- apps/website/src/pages/_404.tsx
+++ apps/website/src/pages/_404.tsx
@@ -1,13 +1,14 @@
 import Section from "../components/Section.js";
 import { usePageTitle } from "../hooks.js";
+import { t } from "../i18n.js";
 import "./_404.css";
 
 export function NotFound() {
-    usePageTitle("404");
+    usePageTitle(t("404.title"));
 
 	return (
-		<Section title="404: Not Found" className="section-404">
-            The page you were looking for could not be found. Maybe it was deleted or the URL is incorrect.
+		<Section title={t("404.title")} className="section-404">
+            {t("404.description")}
 		</Section>
 	);
 }
