b7b1d17817b45af062cc9b47a4e5f7cb54af171c
Elian Doran <contact@eliandoran.me>
2025-10-25 16:41:10 +0300

    chore(website): add list of locales

--- apps/website/src/i18n.ts
+++ apps/website/src/i18n.ts
@@ -2,6 +2,23 @@ import { default as i18next } from "i18next";
 import HttpApi from 'i18next-http-backend';
 import { initReactI18next } from "react-i18next";
 
+interface Locale {
+    id: string;
+    name: string;
+    rtl?: boolean;
+}
+
+export const LOCALES: Locale[] = [
+    { id: "ro", name: "Română" },
+    { id: "zh_TW", name: "繁體中文" },
+    { id: "fr", name: "Français" },
+    { id: "it", name: "Italiano" },
+    { id: "ja", name: "日本語" },
+    { id: "pl", name: "Polski" },
+    { id: "es", name: "Español" },
+    { id: "ar", name: "اَلْعَرَبِيَّةُ", rtl: true },
+];
+
 i18next
     .use(HttpApi)
     .use(initReactI18next);
