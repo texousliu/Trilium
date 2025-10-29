a317331551ac5a123cc04639c3977d2927c3f904
Elian Doran <contact@eliandoran.me>
2025-10-29 19:05:23 +0200

    fix(share): theme toggle flashing while switching between notes

--- packages/share-theme/src/scripts/modules/theme.ts
+++ packages/share-theme/src/scripts/modules/theme.ts
@@ -1,13 +1,14 @@
 const themeRootEl = document.documentElement;
 
+/**
+ * Note:
+ *
+ * - Setting of the .theme-dark or .theme-light is done in the share template's <head> to avoid a flash.
+ * - Setting of the value of the checkbox is also done in the template, near the definition of the input box.
+ */
+
 export default function setupThemeSelector() {
     const themeSwitch: HTMLInputElement = document.querySelector(".theme-selection input")!;
-
-    const themeSelection: HTMLDivElement = document.querySelector(".theme-selection")!;
-    themeSelection.classList.add("no-transition");
-    themeSwitch.checked = (themeRootEl.classList.contains("theme-dark"));
-    setTimeout(() => themeSelection.classList.remove("no-transition"), 400);
-
     themeSwitch?.addEventListener("change", () => {
         const theme = themeSwitch.checked ? "dark" : "light";
         setTheme(theme);
