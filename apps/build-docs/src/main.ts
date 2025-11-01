64428ae76179a2acfa58524a5b08aadf569809c6
Elian Doran <contact@eliandoran.me>
2025-11-01 20:16:59 +0200

    feat(build-docs): clean before building

--- apps/build-docs/src/main.ts
+++ apps/build-docs/src/main.ts
@@ -1,9 +1,19 @@
 import { join } from "path";
 import BuildContext from "./context";
 import buildSwagger from "./swagger";
+import { mkdirSync, rmSync } from "fs";
 
 const context: BuildContext = {
     baseDir: join(__dirname, "../../../site")
 };
 
-buildSwagger(context);
+function main() {
+    // Clean input dir.
+    rmSync(context.baseDir, { recursive: true });
+    mkdirSync(context.baseDir);
+
+    // Start building.
+    buildSwagger(context);
+}
+
+main();
