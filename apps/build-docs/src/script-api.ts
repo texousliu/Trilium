d9a349a5312c59a5e541361e067bc77c92886ef6
Elian Doran <contact@eliandoran.me>
2025-11-01 22:54:42 +0200

    refactor(build-docs): define output for typedoc at script level

--- apps/build-docs/src/script-api.ts
+++ apps/build-docs/src/script-api.ts
@@ -1,11 +1,12 @@
 import { execSync } from "child_process";
 import BuildContext from "./context";
+import { join } from "path";
 
-export default function buildScriptApi({ }: BuildContext) {
+export default function buildScriptApi({ baseDir }: BuildContext) {
     for (const config of [ "backend", "frontend" ]) {
-        execSync(`pnpm typedoc --options typedoc.${config}.json`, {
+        const outDir = join(baseDir, "script-api", config);
+        execSync(`pnpm typedoc --options typedoc.${config}.json --html "${outDir}"`, {
             stdio: "inherit"
-            // * Output dir is set in typedoc.json.
         });
     }
 }
