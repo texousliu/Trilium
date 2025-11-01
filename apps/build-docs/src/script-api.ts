2c1cebfbc3b0ac4d303336102f1ae15e82ed826e
Elian Doran <contact@eliandoran.me>
2025-11-01 22:51:33 +0200

    feat(build-docs): split documentation in two

--- apps/build-docs/src/script-api.ts
+++ apps/build-docs/src/script-api.ts
@@ -2,8 +2,10 @@ import { execSync } from "child_process";
 import BuildContext from "./context";
 
 export default function buildScriptApi({ }: BuildContext) {
-    execSync(`pnpm typedoc`, {
-        stdio: "inherit"
-        // * Output dir is set in typedoc.json.
-    });
+    for (const config of [ "backend", "frontend" ]) {
+        execSync(`pnpm typedoc --options typedoc.${config}.json`, {
+            stdio: "inherit"
+            // * Output dir is set in typedoc.json.
+        });
+    }
 }
