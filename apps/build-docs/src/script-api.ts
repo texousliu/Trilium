4f5be54030b44dc1ad0b60977ead7dc3e58a8aa3
Elian Doran <contact@eliandoran.me>
2025-11-02 18:28:31 +0200

    chore(build-docs): generate types for doc

--- apps/build-docs/src/script-api.ts
+++ apps/build-docs/src/script-api.ts
@@ -2,7 +2,10 @@ import { execSync } from "child_process";
 import BuildContext from "./context";
 import { join } from "path";
 
-export default function buildScriptApi({ baseDir }: BuildContext) {
+export default function buildScriptApi({ baseDir, gitRootDir }: BuildContext) {
+    // Generate types
+    execSync(`pnpm typecheck`, { stdio: "inherit", cwd: gitRootDir });
+
     for (const config of [ "backend", "frontend" ]) {
         const outDir = join(baseDir, "script-api", config);
         execSync(`pnpm typedoc --options typedoc.${config}.json --html "${outDir}"`, {
