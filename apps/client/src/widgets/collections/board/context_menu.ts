90b5282b393b160b1087fc40da683ad154f1db27
Adorian Doran <adorian@esevo.ro>
2025-12-01 13:29:28 +0200

    client/calendar collection: add "Archive note" command to the context menu

--- apps/client/src/widgets/collections/board/context_menu.ts
+++ apps/client/src/widgets/collections/board/context_menu.ts
@@ -2,9 +2,9 @@ import FNote from "../../../entities/fnote";
 import NoteColorPicker from "../../../menus/custom-items/NoteColorPicker";
 import contextMenu, { ContextMenuEvent } from "../../../menus/context_menu";
 import link_context_menu from "../../../menus/link_context_menu";
-import attributes from "../../../services/attributes";
 import branches from "../../../services/branches";
 import dialog from "../../../services/dialog";
+import { getArchiveMenuItem } from "../../../menus/context_menu_utils";
 import { t } from "../../../services/i18n";
 import Api from "./api";
 
@@ -52,7 +52,6 @@ export function openNoteContextMenu(api: Api, event: ContextMenuEvent, note: FNo
                     handler: () => api.changeColumn(note.noteId, columnToMoveTo)
                 })),
             },
-            getArchiveMenuItem(note),
             { kind: "separator" },
             {
                 title: t("board_view.insert-above"),
@@ -65,6 +64,7 @@ export function openNoteContextMenu(api: Api, event: ContextMenuEvent, note: FNo
                 handler: () => api.insertRowAtPosition(column, branchId, "after")
             },
             { kind: "separator" },
+            getArchiveMenuItem(note),
             {
                 title: t("board_view.remove-from-board"),
                 uiIcon: "bx bx-task-x",
@@ -85,20 +85,3 @@ export function openNoteContextMenu(api: Api, event: ContextMenuEvent, note: FNo
     });
 }
 
-function getArchiveMenuItem(note: FNote) {
-    if (!note.isArchived) {
-        return {
-            title: t("board_view.archive-note"),
-            uiIcon: "bx bx-archive",
-            handler: () => attributes.addLabel(note.noteId, "archived")
-        }
-    } else {
-        return {
-            title: t("board_view.unarchive-note"),
-            uiIcon: "bx bx-archive-out",
-            handler: async () => {
-                attributes.removeOwnedLabelByName(note, "archived")
-            }
-        }
-    }
-}
