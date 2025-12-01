f5940cbf70c3efa67063b1f6ec09b8c0c248e4c8
Elian Doran <contact@eliandoran.me>
2025-12-01 23:36:15 +0200

    feat(mobile/split): hide split button if split is already open

--- apps/client/src/widgets/mobile_widgets/mobile_detail_menu.tsx
+++ apps/client/src/widgets/mobile_widgets/mobile_detail_menu.tsx
@@ -21,22 +21,29 @@ export default function MobileDetailMenu() {
                 if (!ntxId) return;
 
                 const noteContext = appContext.tabManager.getNoteContextById(ntxId);
+                const subContexts = noteContext.getMainContext().getSubContexts();
+                const isMainContext = noteContext?.isMainContext();
                 const note = noteContext.note;
 
-                const items: (MenuItem<keyof CommandMappings> | false)[] = [
+                const items: (MenuItem<keyof CommandMappings>)[] = [
                     { title: t("mobile_detail_menu.insert_child_note"), command: "insertChildNote", uiIcon: "bx bx-plus", enabled: note?.type !== "search" },
                     { title: t("mobile_detail_menu.delete_this_note"), command: "delete", uiIcon: "bx bx-trash", enabled: note?.noteId !== "root" },
                     { kind: "separator" },
                     { title: t("mobile_detail_menu.note_revisions"), command: "showRevisions", uiIcon: "bx bx-history" },
                     { kind: "separator" },
-                    { title: t("create_pane_button.create_new_split"), command: "openNewNoteSplit", uiIcon: "bx bx-dock-right" },
-                    !noteContext?.isMainContext() && { title: t("close_pane_button.close_this_pane"), command: "closeThisNoteSplit", uiIcon: "bx bx-x" }
-                ];
+                    subContexts.length < 2 && { title: t("create_pane_button.create_new_split"), command: "openNewNoteSplit", uiIcon: "bx bx-dock-right" },
+                    !isMainContext && { title: t("close_pane_button.close_this_pane"), command: "closeThisNoteSplit", uiIcon: "bx bx-x" }
+                ].filter(i => !!i) as MenuItem<keyof CommandMappings>[];
+
+                const lastItem = items.at(-1);
+                if (lastItem && "kind" in lastItem && lastItem.kind === "separator") {
+                    items.pop();
+                }
 
                 contextMenu.show<keyof CommandMappings>({
                     x: e.pageX,
                     y: e.pageY,
-                    items: items.filter(i => !!i),
+                    items,
                     selectMenuItemHandler: async ({ command }) => {
                         if (command === "insertChildNote") {
                             note_create.createNote(appContext.tabManager.getActiveContextNotePath() ?? undefined);
