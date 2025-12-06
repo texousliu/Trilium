50b0dc178e6c75ccdb84f604418dfa673a048dcd
Elian Doran <contact@eliandoran.me>
2025-12-06 09:27:56 +0200

    chore(regroup): clean up old ETAPI tests
    They are already integrated in apps/server/spec/etapi.

--- _regroup/spec/etapi/app_info.ts
+++
@@ -1,9 +0,0 @@
-import etapi from "../support/etapi.js";
-/* TriliumNextTODO: port to Vitest 
-etapi.describeEtapi("app_info", () => {
-    it("get", async () => {
-        const appInfo = await etapi.getEtapi("app-info");
-        expect(appInfo.clipperProtocolVersion).toEqual("1.0");
-    });
-});
-*/
