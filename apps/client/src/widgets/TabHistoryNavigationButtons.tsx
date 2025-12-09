e3f5b3535aa93c3cd0303772fc728b17d40fcbae
Elian Doran <contact@eliandoran.me>
2025-12-09 14:09:31 +0200

    feat(tab_navigation): functional back/forward buttons

--- apps/client/src/widgets/TabHistoryNavigationButtons.tsx
+++ apps/client/src/widgets/TabHistoryNavigationButtons.tsx
@@ -1,11 +1,21 @@
-import ActionButton from "./react/ActionButton";
 import "./TabHistoryNavigationButtons.css";
+
+import { t } from "../services/i18n";
+import ActionButton from "./react/ActionButton";
 
 export default function TabHistoryNavigationButtons() {
     return (
         <div className="tab-history-navigation-buttons">
-            <ActionButton icon="bx bx-left-arrow-alt" />
-            <ActionButton icon="bx bx-right-arrow-alt" />
+            <ActionButton
+                icon="bx bx-left-arrow-alt"
+                text={t("tab_history_navigation_buttons.go-back")}
+                triggerCommand="backInNoteHistory"
+            />
+            <ActionButton
+                icon="bx bx-right-arrow-alt"
+                text={t("tab_history_navigation_buttons.go-forward")}
+                triggerCommand="forwardInNoteHistory"
+            />
         </div>
-    )
+    );
 }
