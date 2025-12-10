10cb7c8d6a97af86da016c517e0bc1deff7aeb9f
Elian Doran <contact@eliandoran.me>
2025-12-10 12:10:32 +0200

    feat(note_title_details): hide creation dates on hidden notes

--- apps/client/src/widgets/NoteTitleDetails.tsx
+++ apps/client/src/widgets/NoteTitleDetails.tsx
@@ -1,3 +1,5 @@
+import { type ComponentChild } from "preact";
+
 import { t } from "../services/i18n";
 import { formatDateTime } from "../utils/formatters";
 import { useNoteContext } from "./react/hooks";
@@ -7,17 +9,20 @@ import { useNoteMetadata } from "./ribbon/NoteInfoTab";
 export default function NoteTitleDetails() {
     const { note } = useNoteContext();
     const { metadata } = useNoteMetadata(note);
+    const isHiddenNote = note?.noteId.startsWith("_");
+
+    const items: ComponentChild[] = [
+        (!isHiddenNote && metadata?.dateCreated && <li>
+            {t("note_title.created_on", { date: formatDateTime(metadata.dateCreated, "medium", "none")} )}
+        </li>),
+        (!isHiddenNote && metadata?.dateModified && <li>
+            {t("note_title.last_modified", { date: formatDateTime(metadata.dateModified, "medium", "none")} )}
+        </li>)
+    ].filter(item => !!item);
 
     return (
         <div className="title-details">
-            {joinElements([
-                metadata?.dateCreated && <li>
-                    {t("note_title.created_on", { date: formatDateTime(metadata.dateCreated, "medium", "none")} )}
-                </li>,
-                metadata?.dateModified && <li>
-                    {t("note_title.last_modified", { date: formatDateTime(metadata.dateModified, "medium", "none")} )}
-                </li>
-            ], " • ")}
+            {joinElements(items, " • ")}
         </div>
     );
 }
