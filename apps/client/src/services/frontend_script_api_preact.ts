34d5793888c4e35fb024b9337d662ef5a31f12f3
Elian Doran <contact@eliandoran.me>
2025-12-20 21:19:53 +0200

    chore(script/jsx): expose RightPanelWidget

--- apps/client/src/services/frontend_script_api_preact.ts
+++ apps/client/src/services/frontend_script_api_preact.ts
@@ -1,10 +1,14 @@
 import { Fragment, h } from "preact";
 import * as hooks from "preact/hooks";
 
+import RightPanelWidget from "../widgets/sidebar/RightPanelWidget";
+
 export const preactAPI = Object.freeze({
     // Core
     h,
     Fragment,
+
+    RightPanelWidget,
 
     ...hooks
 });
