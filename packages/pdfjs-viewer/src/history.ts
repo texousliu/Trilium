359f398afa87141debd264f0a671141156d48e2a
Elian Doran <contact@eliandoran.me>
2025-12-29 16:19:21 +0200

    feat(pdfjs): debounce saving view config

--- packages/pdfjs-viewer/src/history.ts
+++ packages/pdfjs-viewer/src/history.ts
@@ -2,17 +2,7 @@ export default function interceptViewHistory() {
     const originalSetItem = Storage.prototype.setItem;
     Storage.prototype.setItem = function (key: string, value: string) {
         if (key === "pdfjs.history") {
-            // Parse the history and remove entries that are not relevant.
-            const history = JSON.parse(value);
-            const fingerprint = window.PDFViewerApplication?.pdfDocument?.fingerprints?.[0];
-            if (fingerprint) {
-                history.files = history.files.filter((file: any) => file.fingerprint === fingerprint);
-            }
-
-            window.parent.postMessage({
-                type: "pdfjs-viewer-save-view-history",
-                data: JSON.stringify(history)
-            }, "*");
+            saveHistory(value);
             return;
         }
 
@@ -27,4 +17,26 @@ export default function interceptViewHistory() {
 
         return originalGetItem.call(this, key);
     }
+}
+
+let saveTimeout: number | null = null;
+
+function saveHistory(value: string) {
+    if (saveTimeout) {
+        clearTimeout(saveTimeout);
+    }
+    saveTimeout = window.setTimeout(() => {
+        // Parse the history and remove entries that are not relevant.
+        const history = JSON.parse(value);
+        const fingerprint = window.PDFViewerApplication?.pdfDocument?.fingerprints?.[0];
+        if (fingerprint) {
+            history.files = history.files.filter((file: any) => file.fingerprint === fingerprint);
+        }
+
+        window.parent.postMessage({
+            type: "pdfjs-viewer-save-view-history",
+            data: JSON.stringify(history)
+        }, "*");
+        saveTimeout = null;
+    }, 2_000);
 }
