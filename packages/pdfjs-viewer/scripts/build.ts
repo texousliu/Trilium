667eaca9f2fba00cdfcc81c16dfda411ed932aac
Elian Doran <contact@eliandoran.me>
2025-12-29 12:35:49 +0200

    feat(pdfjs): improve style to better match Trilium

--- packages/pdfjs-viewer/scripts/build.ts
+++ packages/pdfjs-viewer/scripts/build.ts
@@ -5,6 +5,7 @@ const build = new BuildHelper("packages/pdfjs-viewer");
 async function main() {
     build.copy("viewer", "web");
     build.copy("src/custom.mjs", "web/custom.mjs");
+    build.copy("src/custom.css", "web/custom.css");
     build.copy("/node_modules/pdfjs-dist/build/pdf.mjs", "build/pdf.mjs");
     build.copy("/node_modules/pdfjs-dist/build/pdf.worker.mjs", "build/pdf.worker.mjs");
 }
